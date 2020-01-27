8ef112c7e043d2270948a08659ca87633c9171fa
nice0001-angelo <nice0001@gmail.com>
2020-01-27 10:16:32 -0500

    The Initial jin-beta

---
+++ src/main/java/net/jin/JinBetaApplication.java
@@ -0,0 +1,13 @@
+package net.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinBetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jin/JinBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
