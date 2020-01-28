1f1728ff2c337af7bf899ad62b390386b8ac719c
nice0001-angelo <nice0001@gmail.com>
2020-01-28 08:58:52 -0500

    mysql setting and make table

--- src/main/java/net/jin/domain/Board.java
+++ src/main/java/net/jin/domain/Board.java
@@ -19,7 +19,6 @@ import lombok.ToString;
 @ToString
 @Entity
 public class Board {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long bno;
@@ -31,4 +30,42 @@ public class Board {
 	private Timestamp regdate;
 	@UpdateTimestamp
 	private Timestamp updatedate;
+	
+	public Long getBno() {
+		return bno;
+	}
+	public void setBno(Long bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Timestamp regdate) {
+		this.regdate = regdate;
+	}
+	public Timestamp getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(Timestamp updatedate) {
+		this.updatedate = updatedate;
+	}
+
 }
--- src/test/java/net/jin/BoardRepositoryTests.java
+++ src/test/java/net/jin/BoardRepositoryTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import net.jin.domain.Board;
 import net.jin.persistence.BoardRepository;
 
 @RunWith(SpringRunner.class)
@@ -19,5 +20,15 @@ public class BoardRepositoryTests {
 	@Test
 	public void inspect() {
 		
+	}
+	
+	@Test
+	public void insert() {
+		Board board = new Board();
+		board.setTitle("Title");
+		board.setContent("Content");
+		board.setWriter("Writer");
+		
+		boardRepo.save(board);
 	}
 }
