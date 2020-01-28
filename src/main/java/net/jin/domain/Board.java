175176caa4b60308b935f50555cb981734f1cb01
nice0001-angelo <nice0001@gmail.com>
2020-01-28 03:52:47 -0500

    mysql setting and Board object add

---
+++ src/main/java/net/jin/domain/Board.java
@@ -0,0 +1,34 @@
+package net.jin.domain;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long bno;
+	private String title;
+	private String writer;
+	private String content;
+	
+	@CreationTimestamp
+	private Timestamp regdate;
+	@UpdateTimestamp
+	private Timestamp updatedate;
+}
