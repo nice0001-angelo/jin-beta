83de7447e4892665cb0bc2d8ac603282760f4602
nice0001-angelo <nice0001@gmail.com>
2020-01-30 09:48:19 -0500

    initial setting thymeleaf

---
+++ src/main/java/net/jin/controller/MainController.java
@@ -0,0 +1,5 @@
+package net.jin.controller;
+
+public class MainController {
+
+}
---
+++ src/main/java/net/jin/model/Users.java
@@ -0,0 +1,47 @@
+package net.jin.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String user_id;
+	private String user_pw;
+	private String user_name;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_pw() {
+		return user_pw;
+	}
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	
+	
+}
