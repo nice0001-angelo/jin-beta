74229c2450dc21ad0cd8bf6383fb6720e862f1da
nice0001-angelo <nice0001@gmail.com>
2020-01-31 11:12:13 -0500

    add webpage

--- src/main/java/net/jin/controller/MainController.java
+++ src/main/java/net/jin/controller/MainController.java
@@ -1,8 +1,18 @@
 package net.jin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
+	
+	@RequestMapping(value="/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="/joinPage")
+	public String joinPage() {
+		return "join";
+	}
 }
