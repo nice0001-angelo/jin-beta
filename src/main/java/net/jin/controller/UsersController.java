de0533c898320f4d6031b0073b1c3820561750b7
nice0001-angelo <nice0001@gmail.com>
2020-02-03 10:33:04 -0500

    update usersRepository

--- src/main/java/net/jin/controller/UsersController.java
+++ src/main/java/net/jin/controller/UsersController.java
@@ -5,11 +5,19 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import net.jin.repository.UsersRepository;
+import net.jin.service.JoinService;
+
 @Controller
 public class UsersController {
-
+	
+	private UsersRepository usersRepository;
+	
 	@PostMapping(value="/joinRequest")
 	public String joinRequest(HttpServletRequest request) {
+		JoinService joinService = new JoinService();
+		joinService.joinUser(request, usersRepository);
+		
 		return "index";
 
 	}
--- src/main/java/net/jin/service/JoinService.java
+++ src/main/java/net/jin/service/JoinService.java
@@ -7,7 +7,22 @@
  */
 package net.jin.service;
 
+import javax.servlet.http.HttpServletRequest;
+
+import net.jin.model.Users;
+import net.jin.repository.UsersRepository;
+
 public class JoinService {
+	public void joinUser(HttpServletRequest request, UsersRepository usersRepository) {
+			String userId = request.getParameter("user_id");
+			String userPw = request.getParameter("user_pw");
+			String userName = request.getParameter("user_name");
+			
+			Users users = new Users(); // object make
+			users.setUser_id(userId);
+			users.setUser_pw(userPw);
+			users.setUser_name(userName);
+	}
 	
 	
 }
