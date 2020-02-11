dfae1c959502c65ff84006410ea6aaf3bde4b462
nice0001-angelo <nice0001@gmail.com>
2020-02-10 20:53:49 -0500

    beta update

--- src/main/java/net/jin/controller/MainController.java
+++ src/main/java/net/jin/controller/MainController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	@RequestMapping(value="")
+	@RequestMapping(value="/")
 	public String index() {
 		return "index";
 	}
