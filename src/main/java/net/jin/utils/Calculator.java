7aced6e351b93041b62843523b0939c8624e48bd
nice0001-angelo <nice0001@gmail.com>
2020-07-23 03:46:13 -0400

    Junit testing

---
+++ src/main/java/net/jin/utils/Calculator.java
@@ -0,0 +1,8 @@
+package net.jin.utils;
+
+public class Calculator {
+	public int plus(int x, int y) {
+		return x+y;
+	}
+
+}
--- src/test/java/net/jin/JinBetaApplicationTests.java
+++ src/test/java/net/jin/JinBetaApplicationTests.java
@@ -1,13 +1,27 @@
 package net.jin;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import net.jin.utils.Calculator;
 
 @SpringBootTest
 class JinBetaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void plus() throws Exception{
+		//given
+		Calculator calculator = new Calculator();
+		
+		//when
+		int result = calculator.plus(2,5); 
+		
+		//then
+		assertThat(result, is(7));
 	}
 
 }
