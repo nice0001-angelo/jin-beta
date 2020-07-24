25215f2b9f397841dc169fa779092b9ff3492ff3
nice0001-angelo <nice0001@gmail.com>
2020-07-24 04:28:03 -0400

    testing

--- src/main/java/net/jin/JinBetaApplication.java
+++ src/main/java/net/jin/JinBetaApplication.java
@@ -37,7 +37,7 @@ public class JinBetaApplication {
 		 *************************************/
 		//Date
 		System.out.println();
-		System.out.println("*************Start of System************ ");
+		System.out.println("*************Start of Date************ ");
 		Date now = new Date();
 		String strNow1 = now.toString();
 		System.out.println(strNow1);
@@ -45,7 +45,13 @@ public class JinBetaApplication {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 hh시 mm분 ss초");
 		String strNow2 = sdf.format(now);
 		System.out.println(strNow2);
-				
+
+		System.out.println();
+		System.out.println("*************End of Date************ ");
+		
+		//Calendar
+		System.out.println();
+		System.out.println("*************Start of System************ ");
 		
 		//Time check
 		System.out.println();
