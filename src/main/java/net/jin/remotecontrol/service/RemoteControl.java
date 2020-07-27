02c6812a950b83b73a4a691bc902ed573ddc3ed3
nice0001-angelo <nice0001@gmail.com>
2020-07-27 06:33:51 -0400

    update

--- src/main/java/net/jin/remotecontrol/service/RemoteControl.java
+++ src/main/java/net/jin/remotecontrol/service/RemoteControl.java
@@ -1,6 +1,12 @@
 package net.jin.remotecontrol.service;
 
 public interface RemoteControl {
+	//Constant field
 	public static final int MAX_VOLUME = 10;
 	public static final int MIN_VOLUME = 0;
+	
+	//Abstract Method
+	public abstract void turnOn();
+	public abstract void turnOff();
+	public abstract void setVolumn();
 }
