0fd70feede5760393fadc8feef8c1feb3ef0dda9
nice0001-angelo <nice0001@gmail.com>
2020-07-28 05:47:52 -0400

    refactoring

--- src/main/java/net/jin/reomtecontrol/service/impl/Television.java
+++ src/main/java/net/jin/reomtecontrol/service/impl/Television.java
@@ -15,8 +15,14 @@ public class Television implements RemoteControl{
     	System.out.println("TV를 끕니다");
     }
     
-    public void setVolume(int Volume) {
-    	
+    public void setVolume(int volume) {
+    	if(volume > RemoteControl.MAX_VOLUME) {
+    		this.volume = RemoteControl.MAX_VOLUME;
+    	}else if(volume < RemoteControl.MIN_VOLUME) {
+    		this.volume = RemoteControl.MIN_VOLUME;
+    	}else {
+    		this.volume =  volume;
+    	}
+    	System.out.println("현재 TV 볼륨: "+this.volume);
     }
-
 }
