401dcc41ab506daf99cf957ad834bd12d0b526e4
nice0001-angelo <nice0001@gmail.com>
2020-07-31 03:08:36 -0400

    refactoring

---
+++ src/main/java/com/jin/TryCatchFinallyRuntimeException.java
@@ -0,0 +1,27 @@
+package com.jin;
+
+public class TryCatchFinallyRuntimeException {
+
+	public static void main(String[] args) {
+		String data1 = null;
+		String data2 = null;
+		try {
+			data1 = args[0];
+			data2 = args[1];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("매개변수의 개수가 부족합니다. 최소 2개 필요");
+			return;
+		}
+		
+		try {
+			int value1 = Integer.parseInt(data1);
+			int value2 = Integer.parseInt(data2);
+			int result = value1 + value2;
+			System.out.println(value1 + " + " + value2 + " = " + result);
+		} catch (Exception e) {
+			System.out.println("숫자로 변환할 수 없습니다");
+		} finally {
+			System.out.println("다시 실행하세요.");
+		}
+	}
+}
