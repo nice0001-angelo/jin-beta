2016b7d61874f6c0b77f0cba46650eb79460d8f5
nice0001-angelo <nice0001@gmail.com>
2020-08-02 02:16:13 -0400

    refactoring

--- src/main/java/com/jin/BeepPrint.java
+++ src/main/java/com/jin/BeepPrint.java
@@ -6,7 +6,23 @@ public class BeepPrint {
 
 	public static void main(String[] args) {
 		Toolkit toolkit = java.awt.Toolkit.getDefaultToolkit();
-
+		for(int i=0; i<5; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("띵");
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
 	}
 
 }
---
+++ src/main/java/com/jin/BeepTask.java
@@ -0,0 +1,18 @@
+package com.jin;
+
+import java.awt.Toolkit;
+
+public class BeepTask implements Runnable{
+	public void run() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for(int i=0; i<5; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+
+}
