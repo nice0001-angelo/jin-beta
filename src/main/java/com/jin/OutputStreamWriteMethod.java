7dff2ebdbb5f209fdd2cff9cee95bcbac0edb039
nice0001-angelo <nice0001@gmail.com>
2020-08-02 07:14:36 -0400

    refactoring

--- src/main/java/com/jin/OutputStreamWriteMethod.java
+++ src/main/java/com/jin/OutputStreamWriteMethod.java
@@ -16,15 +16,21 @@ public class OutputStreamWriteMethod {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception{
-		OutputStream os = new FileOutputStream("C:\\Temp/test1.txt");
+//		OutputStream os = new FileOutputStream("C:\\Temp/test1.txt");
+//		
+//		byte a = 10;
+//		byte b = 20;
+//		byte c = 30;
+//		
+//		os.write(a);
+//		os.write(b);
+//		os.write(c);
 		
-		byte a = 10;
-		byte b = 20;
-		byte c = 30;
+		OutputStream os = new FileOutputStream("C:\\Temp/test2.txt");
 		
-		os.write(a);
-		os.write(b);
-		os.write(c);
+		byte[] array = {10,20,30,40,50};
+		
+		os.write(array, 1, 3);
 		
 		os.flush();
 		os.close();
