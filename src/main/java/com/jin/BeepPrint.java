ee0d2f9b0a768de31fa7d5531ffafabed16516f8
nice0001-angelo <nice0001@gmail.com>
2020-08-01 21:22:45 -0400

    refactoring

---
+++ src/main/java/com/jin/BeepPrint.java
@@ -0,0 +1,12 @@
+package com.jin;
+
+import java.awt.*;
+
+public class BeepPrint {
+
+	public static void main(String[] args) {
+		Toolkit toolkit = java.awt.Toolkit.getDefaultToolkit();
+
+	}
+
+}
