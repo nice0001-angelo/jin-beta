7bab5a036dcf92e54300009464018afa5391c043
nice0001-angelo <nice0001@gmail.com>
2020-08-03 03:18:10 -0400

    refactoring

--- src/main/java/com/jin/WriterWriteMethod.java
+++ src/main/java/com/jin/WriterWriteMethod.java
@@ -3,6 +3,9 @@
  */
 package com.jin;
 
+import java.io.FileWriter;
+import java.io.Writer;
+
 /**
  * @author njh
  *
@@ -12,8 +15,19 @@ public class WriterWriteMethod {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		Writer writer = new FileWriter("C:\\Temp/test7.txt");
+		
+		char a = 'A';
+		char b = 'B';
+		char c = 'C';
+		
+		writer.write(a);
+		writer.write(b);
+		writer.write(c);
+		
+		writer.flush();
+		writer.close();
 
 	}
 
