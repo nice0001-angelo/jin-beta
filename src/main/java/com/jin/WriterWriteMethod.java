76a2414c42a6c0a98583d936d8c8fc9b0805d012
nice0001-angelo <nice0001@gmail.com>
2020-08-03 03:07:01 -0400

    add Writer

---
+++ src/main/java/com/jin/WriterWriteMethod.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin;
+
+/**
+ * @author njh
+ *
+ */
+public class WriterWriteMethod {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
