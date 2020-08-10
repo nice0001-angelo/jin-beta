981c17e190049f580dced7e186100249b9f98e19
nice0001-angelo <nice0001@gmail.com>
2020-08-10 13:22:20 -0400

    refactoring

--- src/main/java/com/jin/stream/ObjectStream.java
+++ src/main/java/com/jin/stream/ObjectStream.java
@@ -3,6 +3,7 @@
  */
 package com.jin.stream;
 
+import java.io.*;
 import java.text.*;
 import java.util.*;
 
@@ -30,7 +31,17 @@ public class ObjectStream {
 
 	public static void writeList() throws Exception{
 		List<Board> list = new ArrayList<>();
+		
+		list.add(new Board(1, "title1", "content1", "writer1", new Date()));
+		list.add(new Board(2, "title2", "content2", "writer2", new Date()));
+		list.add(new Board(3, "title3", "content3", "writer3", new Date()));
+		list.add(new Board(4, "title4", "content4", "writer4", new Date()));
+		
+		FileOutputStream fos = new FileOutputStream("C:\\Temp/board.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(list);
 	}
+	
 	
 	public static List<Board> readList() throws Exception{
 		
