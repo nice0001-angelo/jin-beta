e935ea719b3ea7a60321ecce381056959cdb0fd4
nice0001-angelo <nice0001@gmail.com>
2020-08-11 01:51:59 -0400

    add ThreadName

---
+++ src/main/java/com/jin/thread/ThreadName.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.thread;
+
+/**
+ * @author njh
+ *
+ */
+public class ThreadName {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
