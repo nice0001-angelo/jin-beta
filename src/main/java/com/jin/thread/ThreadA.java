76ecd583aa4acfb8cfd328314278133b0d56c4f4
nice0001-angelo <nice0001@gmail.com>
2020-08-12 03:36:46 -0400

    add ThreadA

---
+++ src/main/java/com/jin/thread/ThreadA.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.jin.thread;
+
+/**
+ * @author njh
+ *
+ */
+public class ThreadA extends Thread {
+	public ThreadA() {
+		setName("ThreadA");
+	}
+	
+	public void run() {
+		for(int i=0; i<2; i++) {
+			System.out.println(getName()+"가 출력한 내용");
+		}
+	}
+
+}
--- src/main/java/com/jin/thread/ThreadName.java
+++ src/main/java/com/jin/thread/ThreadName.java
@@ -14,8 +14,11 @@ public class ThreadName {
 	 */
 	public static void main(String[] args) {
 		Thread mainThread = Thread.currentThread();
+		System.out.println("프로그램 시작 쓰레드 이름: "+mainThread.getName());
 		
-
+		ThreadA threadA = new ThreadA();
+		System.out.println("작업 쓰레드 이름: " + threadA.getName());
+		threadA.start();
 	}
 
 }
