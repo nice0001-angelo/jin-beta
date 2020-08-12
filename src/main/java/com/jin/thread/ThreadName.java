7dbe6a38af04a65dba613e2080b4fe5111a63f26
nice0001-angelo <nice0001@gmail.com>
2020-08-12 03:30:51 -0400

    refactoring

--- src/main/java/com/jin/thread/ThreadName.java
+++ src/main/java/com/jin/thread/ThreadName.java
@@ -13,7 +13,8 @@ public class ThreadName {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Thread mainThread = Thread.currentThread();
+		
 
 	}
 
