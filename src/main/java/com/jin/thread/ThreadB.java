f738f1894ae9070f2839e2ddd6920c5c8499df05
nice0001-angelo <nice0001@gmail.com>
2020-08-12 04:07:24 -0400

    refactoring

--- src/main/java/com/jin/thread/ThreadB.java
+++ src/main/java/com/jin/thread/ThreadB.java
@@ -8,6 +8,9 @@ package com.jin.thread;
  *
  */
 public class ThreadB extends Thread{
+	
+	//setName() 하지 않으면 자동으로 이름이 Thread-n 형태로 대입된다
+	
 	public void run() {
 		for(int i=0; i<2; i++) {
 			System.out.println(getName() + "가 출력한 내용");
