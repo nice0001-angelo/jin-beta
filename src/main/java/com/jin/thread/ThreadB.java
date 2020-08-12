6b413630968fa8afbe519118a45370b613e9beb2
nice0001-angelo <nice0001@gmail.com>
2020-08-12 04:06:15 -0400

    refactoring

---
+++ src/main/java/com/jin/thread/ThreadB.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.jin.thread;
+
+/**
+ * @author njh
+ *
+ */
+public class ThreadB extends Thread{
+	public void run() {
+		for(int i=0; i<2; i++) {
+			System.out.println(getName() + "가 출력한 내용");
+		}
+	}
+}
--- src/main/java/com/jin/thread/ThreadName.java
+++ src/main/java/com/jin/thread/ThreadName.java
@@ -19,6 +19,9 @@ public class ThreadName {
 		ThreadA threadA = new ThreadA();
 		System.out.println("작업 쓰레드 이름: " + threadA.getName());
 		threadA.start();
+		
+		ThreadB threadB = new ThreadB();
+		System.out.println("작업 스레드 이름: " + threadB.getName());
+		threadB.start();
 	}
-
 }
