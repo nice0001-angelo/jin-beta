3e1a200b64a657619247416455eb251af193fe76
nice0001-angelo <nice0001@gmail.com>
2020-08-15 15:58:19 -0400

    update

--- src/main/java/com/jin/thread/Calculator.java
+++ src/main/java/com/jin/thread/Calculator.java
@@ -8,5 +8,20 @@ package com.jin.thread;
  *
  */
 public class Calculator {
+	private int memory;
+
+	public int getMemory() {
+		return memory;
+	}
+
+	public void setMemory(int memory) {
+		this.memory = memory;
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			System.out.println(Thread.currentThread().getName() + ": " + this.memory);
+		}
+	}
+	
 
 }
