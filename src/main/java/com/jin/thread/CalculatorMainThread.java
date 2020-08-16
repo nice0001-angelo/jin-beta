7ef489dec1e0fa781c8c347d45d00b06a026432e
nice0001-angelo <nice0001@gmail.com>
2020-08-15 22:40:38 -0400

    refactoring

--- src/main/java/com/jin/thread/CalculatorMainThread.java
+++ src/main/java/com/jin/thread/CalculatorMainThread.java
@@ -18,7 +18,10 @@ public class CalculatorMainThread {
 		CalculatorUser1 calculatorUser1 = new CalculatorUser1();
 		calculatorUser1.setCalculator(calculator);
 		calculatorUser1.start();
-
+		
+		CalculatorUser2 calculatorUser2 = new CalculatorUser2();
+		calculatorUser2.setcalculator(calculator);
+		calculatorUser2.start();
 	}
 
 }
