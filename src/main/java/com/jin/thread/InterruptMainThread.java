3aa804c0a458c5995097237e9108ca4e1c72951e
nice0001-angelo <nice0001@gmail.com>
2020-08-16 17:05:17 -0400

    refactoring

--- src/main/java/com/jin/thread/InterruptMainThread.java
+++ src/main/java/com/jin/thread/InterruptMainThread.java
@@ -13,8 +13,15 @@ public class InterruptMainThread {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Thread thread = new InterruptPrintThread1();
+		thread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+		}
+		thread.interrupt();
 	}
 
 }
