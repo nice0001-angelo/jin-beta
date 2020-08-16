8b1d1ff8845dd33196986904c2f85503ac05fa91
nice0001-angelo <nice0001@gmail.com>
2020-08-16 15:11:01 -0400

    add method

---
+++ src/main/java/com/jin/thread/StopFlagMainThread.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.thread;
+
+/**
+ * @author njh
+ *
+ */
+public class StopFlagMainThread {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/jin/thread/StopFlagPrintThread.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.jin.thread;
+
+/**
+ * @author njh
+ *
+ */
+public class StopFlagPrintThread extends Thread {
+	private boolean stop;
+	
+	public void setStop(boolean stop) {
+		this.stop = stop;
+	}
+	
+	public void run() {
+		while(!stop) {
+			System.out.println("실행중");
+		}
+		System.out.println("자원정리");
+		System.out.println("실행종료");
+	}
+}
