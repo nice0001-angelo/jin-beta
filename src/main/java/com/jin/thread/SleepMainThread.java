e9ab0fe1bbf430fdf29572a83adb2effb7c6dc3f
nice0001-angelo <nice0001@gmail.com>
2020-08-16 15:05:12 -0400

    add class

---
+++ src/main/java/com/jin/thread/SleepMainThread.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.jin.thread;
+
+import java.awt.*;
+
+/**
+ * @author njh
+ *
+ */
+public class SleepMainThread {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for(int i=0; i<10; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO: handle exception
+			}
+		}
+	
+	}
+
+}
