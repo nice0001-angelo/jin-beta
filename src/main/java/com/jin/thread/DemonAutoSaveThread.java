c628b8768c45ec26dc759cbc8fffc7ce64ea0f25
nice0001-angelo <nice0001@gmail.com>
2020-08-18 17:14:25 -0400

    update

--- src/main/java/com/jin/thread/DemonAutoSaveThread.java
+++ src/main/java/com/jin/thread/DemonAutoSaveThread.java
@@ -7,6 +7,21 @@ package com.jin.thread;
  * @author njh
  *
  */
-public class DemonAutoSaveThread {
+public class DemonAutoSaveThread extends Thread{
+	public void save() {
+		System.out.println("작업 네용을 저장함.");
+	}
+	
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				break;
+			}
+			save();
+		}
+	}
 
 }
