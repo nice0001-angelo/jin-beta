42146bcafd2069784aac669e3aa742dcf0f3105c
nice0001-angelo <nice0001@gmail.com>
2020-08-21 08:12:19 -0400

    refactoring

--- src/main/java/com/jin/singleton/SingletonMain.java
+++ src/main/java/com/jin/singleton/SingletonMain.java
@@ -12,7 +12,7 @@ public class SingletonMain {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String...args) {
 		//Singleton obj1 = new Singleton(); //private constructor
 		
 		Singleton obj1 = Singleton.getInstace();
