6cc5c489c3e7386013989a1852f803f92065432e
nice0001-angelo <nice0001@gmail.com>
2020-08-22 06:10:20 -0400

    refactoring

---
+++ src/main/java/com/jin/shuffle/CollectionsShuffle.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.jin.shuffle;
+
+import java.util.*;
+
+/**
+ * @author njh
+ *
+ */
+public class CollectionsShuffle {
+
+	/**
+	 * @param args
+	 */
+	private static final int DECK_SIZE = 52;		
+	
+	public static void main(String...args) {
+		ArrayList<Integer> deck = new ArrayList<>();
+		
+		for(int i=0; i<DECK_SIZE; ++i) {
+			deck.add(i);
+		}
+		
+		Collections.shuffle(deck);
+		
+		System.out.println(deck);
+	}
+
+}
--- src/main/java/com/jin/shuffle/Shuffle.java
+++ src/main/java/com/jin/shuffle/Shuffle.java
@@ -3,8 +3,6 @@
  */
 package com.jin.shuffle;
 
-import java.util.*;
-
 /**
  * @author njh
  *
@@ -14,18 +12,9 @@ public class Shuffle {
 	/**
 	 * @param args
 	 */
-	private static final int DECK_SIZE = 52;		
-	
 	public static void main(String[] args) {
-		ArrayList<Integer> deck = new ArrayList<>();
-		
-		for(int i=0; i<DECK_SIZE; ++i) {
-			deck.add(i);
-		}
-		
-		Collections.shuffle(deck);
-		
-		System.out.println(deck);
+		// TODO Auto-generated method stub
+
 	}
 
 }
