e4e0bfa4979bcd3c1e6e2633996d42e55ab38e5f
nice0001-angelo <nice0001@gmail.com>
2020-08-22 19:34:37 -0400

    refactoring

--- src/main/java/com/jin/shuffle/Shuffle.java
+++ src/main/java/com/jin/shuffle/Shuffle.java
@@ -15,7 +15,7 @@ public class Shuffle {
 	 * @param args
 	 */
 	
-	private static final int SIZE_DECK = 100;
+	private static final int SIZE_DECK = 52	;
 	
 	public static void main(String[] args) {
 		ArrayList<Integer> deck = new ArrayList<Integer>();
@@ -23,16 +23,19 @@ public class Shuffle {
 		for(int i=0; i<SIZE_DECK; ++i) {
 			deck.add(i);
 		}
-		System.out.println("deck: "+deck);
-		System.out.println("deck.size(): "+deck.size());
+		System.out.println("before deck: " + deck);
+		
 		
 		ArrayList<Integer> shuffledDeck = new ArrayList<Integer>();
 		
-		double ran = Math.random();
-		System.out.println("ran: "+ran);
+		while(deck.size() > 0) {
+			double ran = Math.random();		
+			int index =  (int)(ran*deck.size());
+			shuffledDeck.add(deck.remove(index));
+		}
+		System.out.println("after deck: " + deck);
+		System.out.println("shuffledDeck: "+shuffledDeck);
 		
-		int index =  (int)(ran*deck.size());
-		System.out.println("index: "+index);
 	}
 
 }
