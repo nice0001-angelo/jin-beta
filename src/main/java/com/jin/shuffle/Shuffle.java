31a6f931946e4fbd4c041ced74e9130f028c1a70
nice0001-angelo <nice0001@gmail.com>
2020-08-21 22:36:14 -0400

    add shuffle

---
+++ src/main/java/com/jin/shuffle/Shuffle.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.shuffle;
+
+/**
+ * @author njh
+ *
+ */
+public class Shuffle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
