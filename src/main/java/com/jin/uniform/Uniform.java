da4f4e8be6330d96a8f8a9101c4a4552aa156ae8
nice0001-angelo <nice0001@gmail.com>
2020-08-23 08:10:45 -0400

    refactoring

--- src/main/java/com/jin/uniform/Uniform.java
+++ src/main/java/com/jin/uniform/Uniform.java
@@ -15,40 +15,41 @@ public class Uniform {
 	 * @param args
 	 */
 	static int n = 5;
-	static int[] lost = {2,4};
-	static int[] reserve = {1,3,5};
-	
+	static int[] lost = { 2, 4 };
+	static int[] reserve = { 1, 3, 5 };
+
 	public static void main(String[] args) {
 
-		Solution(n, lost, reserve);
+		int sol = Solution(n, lost, reserve);
+		System.out.println("The Answer is: "+sol);
 	}
 
 	public static int Solution(int n, int[] lost, int[] reserve) {
 		int[] people = new int[n];
 		int answer = n;
-		
-		for(int l : lost) {
-			people[l-1]--;
+
+		for (int l : lost) {
+			people[l - 1]--;
 		}
-		
-		for(int r : reserve) {
-			people[r-1]++;
+
+		for (int r : reserve) {
+			people[r - 1]++;
 		}
-		
-		for(int i=0; i<people.length; i++) {
-			if(people[i] == -1){
-				if(i-1>=0 && people[i-1]==1) {
+
+		for (int i = 0; i < people.length; i++) {
+			if (people[i] == -1) {
+				if (i - 1 >= 0 && people[i - 1] == 1) {
 					people[i]++;
-					people[i-1]--;
-				}else if(i+1<){
-					
-				}
-				
+					people[i - 1]--;
+				} else if (i + 1 < people.length && people[i + 1] == 1) {
+					people[i]++;
+					people[i + 1]--;
+				} else
+					answer--;
 			}
 		}
-		
-		
+
 		return answer;
-		
+
 	}
 }
