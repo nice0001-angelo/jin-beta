11e2e7c1c6cd8924de58536da0e57c1f6053ae97
nice0001-angelo <nice0001@gmail.com>
2020-08-22 21:59:56 -0400

    add Uniform

---
+++ src/main/java/com/jin/uniform/Uniform.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.jin.uniform;
+
+import net.jin.utils.*;
+
+/**
+ * @author njh
+ *
+ */
+public class Uniform {
+
+	/**
+	 * @param args
+	 */
+	static int n = 10;
+	static int[] lost = {1,2,3,4,5};
+	static int[] reserve = {2,4};
+	
+	public static void main(String[] args) {
+
+		Solution(n, lost, reserve);
+	}
+
+	public static int Solution(int n, int[] lost, int[] reserve) {
+		int answer = 0;
+		return answer;
+		
+	}
+}
