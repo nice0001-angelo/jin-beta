a6d18025b9e353db3f88ca374e18d38f74a6bfc7
nice0001-angelo <nice0001@gmail.com>
2020-08-24 23:45:52 -0400

    refactoring

--- src/main/java/com/jin/programmers/lvl1/KthNumber.java
+++ src/main/java/com/jin/programmers/lvl1/KthNumber.java
@@ -12,6 +12,10 @@ public class KthNumber {
 	/**
 	 * @param args
 	 */
+	
+	int[] array = {1,5,2,6,3,7,4};
+	int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
