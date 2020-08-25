d841b85089bd2233cdca71b09eea86be7f7ced1c
nice0001-angelo <nice0001@gmail.com>
2020-08-24 23:27:02 -0400

    refactoring

---
+++ src/main/java/com/jin/programmers/lvl1/KthNumber.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.programmers.lvl1;
+
+/**
+ * @author njh
+ *
+ */
+public class KthNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
