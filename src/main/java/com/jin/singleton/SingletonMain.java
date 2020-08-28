142a15c402dc25f315ce8fa7182bcf3c271ce2a5
nice0001-angelo <nice0001@gmail.com>
2020-08-28 13:00:48 -0400

    refactoring

--- src/main/java/com/jin/singleton/SingletonMain.java
+++ src/main/java/com/jin/singleton/SingletonMain.java
@@ -15,13 +15,13 @@ public class SingletonMain {
 	public static void main(String...args) {
 		//Singleton obj1 = new Singleton(); //private constructor
 
-		//Singlton check
+		//Singlton check good
 		Singleton obj1 = Singleton.getInstace();
 		Singleton obj2 = Singleton.getInstace();
 		
 		if(obj1.equals(obj2)) {
 			System.out.println("같은  Singleton 객체입니다");
-		} else {
+		} else  {
 			System.out.println("다른  Singleton 객체입니다");
 		}
 	}
