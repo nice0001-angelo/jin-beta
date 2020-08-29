de71fc7bfe2d8558217057f97a671d8b5fb19dbc
nice0001-angelo <nice0001@gmail.com>
2020-08-29 16:35:27 -0400

    refactoring

--- src/main/java/com/jin/singleton/Speaker.java
+++ src/main/java/com/jin/singleton/Speaker.java
@@ -10,5 +10,10 @@ package com.jin.singleton;
 public class Speaker {
 	private static Speaker speaker = new Speaker();
 	
-	
+	private Speaker() {
+	}
+
+	private static Speaker getInstance() {
+		return speaker;
+	}
 }
