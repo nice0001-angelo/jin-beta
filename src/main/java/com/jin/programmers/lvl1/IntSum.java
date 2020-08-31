6ee0f34825c3728bddee8ab2ff9a9c42399ebcf9
nice0001-angelo <nice0001@gmail.com>
2020-08-31 00:10:21 -0400

    refactoring

--- src/main/java/com/jin/programmers/lvl1/IntSum.java
+++ src/main/java/com/jin/programmers/lvl1/IntSum.java
@@ -13,13 +13,20 @@ public class IntSum {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		IntSum intsum = new IntSum();
+		long result = 0;
+		result = intsum.solution(3,5);
+		System.out.println("result is: "+result);
 
 	}
 
 	
     public long solution(int a, int b) {
     	long answer = 0;
+    	
+    	for(int i = a; i <= b; i++) {
+    		answer = i+answer;
+    	}
     	return answer;    	
     }
 }
