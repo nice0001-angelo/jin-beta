6d97641e49930d7a5009c07ef6a7492bb08deadc
nice0001-angelo <nice0001@gmail.com>
2020-09-21 21:13:19 -0400

    refactoring

--- src/main/java/com/jin/programmers/lvl1/StringSorting.java
+++ src/main/java/com/jin/programmers/lvl1/StringSorting.java
@@ -28,7 +28,7 @@ public class StringSorting {
 			System.out.println(str.getClass().getName());
 			System.out.println("");
 			System.out.println(str.getClass().getName());
-
+			System.out.println(str.getClass().getName());
 		}
 		
 		
