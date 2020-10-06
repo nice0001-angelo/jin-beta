a8109b5b49b41e108d3e2c0341f15bfa9f709e5c
nice0001-angelo <nice0001@gmail.com>
2020-10-06 08:42:33 -0400

    update

--- src/main/java/com/jin/algorithm/stage1/Max3.java
+++ src/main/java/com/jin/algorithm/stage1/Max3.java
@@ -16,7 +16,23 @@ public class Max3 {
 	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
+		
+		System.out.println("find the maximum number from 3 numbers");
+		System.out.println("the number value a: ");
+		int a = scan.nextInt();
+		System.out.println("the number value b: ");
+		int b = scan.nextInt();
+		System.out.println("the number value c: ");
+		int c = scan.nextInt();
+		
+		int max = a;
+		if(a>b) {
+			max = a;
+		} else if (b > c){
+			max = b;
+		}
+		
+		System.out.println("the maximum value from a, b, c is "+max);
 	}
 
 }
