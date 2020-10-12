a298695791cf5637893ada8d5402e2065605dfe3
nice0001-angelo <nice0001@gmail.com>
2020-10-12 01:53:43 -0400

    I am jimmy

--- src/main/java/com/jin/algorithm/stage1/Median.java
+++ src/main/java/com/jin/algorithm/stage1/Median.java
@@ -12,6 +12,21 @@ public class Median {
 	/**
 	 * @param args
 	 */
+	
+	static int med3(int a, int b, int c) {
+		try {
+			if(a>=b) {
+				if(b>=c) {
+					return b;
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
