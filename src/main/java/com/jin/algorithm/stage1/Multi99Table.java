989e7a9698519588115ff809b4468f894bb6402b
nice0001-angelo <nice0001@gmail.com>
2020-10-15 04:12:19 -0400

    refactoring

---
+++ src/main/java/com/jin/algorithm/stage1/Multi99Table.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.jin.algorithm.stage1;
+
+/**
+ * @author njh
+ *
+ */
+public class Multi99Table {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("=============99============");
+		
+		try {
+			for(int i=1; i<=9; i++) {
+				for(int j=1; j<=9; j++) {
+					System.out.printf("%3d", i*j);
+				}
+				System.out.println();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+}
