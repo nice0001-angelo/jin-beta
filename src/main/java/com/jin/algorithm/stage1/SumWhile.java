7975a49053b1b7baa07c10e439477df871aeb266
nice0001-angelo <nice0001@gmail.com>
2020-10-15 01:41:52 -0400

    refactoring

--- src/main/java/com/jin/algorithm/stage1/SumWhile.java
+++ src/main/java/com/jin/algorithm/stage1/SumWhile.java
@@ -16,7 +16,22 @@ public class SumWhile {
 	 */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
+		System.out.println("From 1 to n sum");
+		System.out.println("Enter the number: ");
+		int n = input.nextInt();
+		
+		int sum = 0;
+		int i = 1;		
+		
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
+		
+		whi
 	}
 
 }
