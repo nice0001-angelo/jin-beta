b29f27f1abaaf9742b9e4bcf184159a2518ddda2
nice0001-angelo <nice0001@gmail.com>
2020-12-22 03:31:01 -0500

    update

--- src/main/java/com/jin/algorithm/stage2/MaxOfArray.java
+++ src/main/java/com/jin/algorithm/stage2/MaxOfArray.java
@@ -35,9 +35,11 @@ public class MaxOfArray {
 	static int maxOf(int[] a) {
 		int max = a[0];
 		for(int i=0; i<a.length; i++) {
-			
+			if(a[i]>max) {
+				max = a[i];
+			}
 		}
-		return 0;
+		return max;
 	}
 
 }
