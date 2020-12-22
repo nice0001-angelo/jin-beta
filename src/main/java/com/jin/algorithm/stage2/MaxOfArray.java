e42f8f1a80e8bd9f84a237545a9dc6f8b1264357
nice0001-angelo <nice0001@gmail.com>
2020-12-22 03:08:25 -0500

    update

--- src/main/java/com/jin/algorithm/stage2/MaxOfArray.java
+++ src/main/java/com/jin/algorithm/stage2/MaxOfArray.java
@@ -21,8 +21,23 @@ public class MaxOfArray {
 		System.out.println("Input Number of Person: ");
 		int num = stdIn.nextInt(); //배열의 요솟수를 입력 받음
 		
-		int[] height = new int[num];
-
+		int[] height = new int[num]; //요솟수가 num인 배열을 생성
+		
+		for(int i=0; i<num; i++) {
+			System.out.println("height["+i+"]:");
+			height[i] = stdIn.nextInt();
+		}
+		
+		System.out.println("Maximum value:"+maxOf(height));
+		
+	}
+	
+	static int maxOf(int[] a) {
+		int max = a[0];
+		for(int i=0; i<a.length; i++) {
+			
+		}
+		return 0;
 	}
 
 }
