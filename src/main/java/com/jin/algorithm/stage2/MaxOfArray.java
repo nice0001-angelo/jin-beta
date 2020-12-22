a716779a81275ec781f23cef8895c645c604c029
nice0001-angelo <nice0001@gmail.com>
2020-12-22 02:29:15 -0500

    update

--- src/main/java/com/jin/algorithm/stage2/MaxOfArray.java
+++ src/main/java/com/jin/algorithm/stage2/MaxOfArray.java
@@ -3,6 +3,8 @@
  */
 package com.jin.algorithm.stage2;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -13,7 +15,13 @@ public class MaxOfArray {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("Finding max number: ");
+		System.out.println("Input Number of Person: ");
+		int num = stdIn.nextInt(); //배열의 요솟수를 입력 받음
+		
+		int[] height = new int[num];
 
 	}
 
