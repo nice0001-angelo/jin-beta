d06e1c36c28fc7070fe4423d2b9ae2a21f492e7d
nice0001-angelo <nice0001@gmail.com>
2020-12-22 01:19:05 -0500

    add MaxOfArray

---
+++ src/main/java/com/jin/algorithm/stage2/MaxOfArray.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.algorithm.stage2;
+
+/**
+ * @author njh
+ *
+ */
+public class MaxOfArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
