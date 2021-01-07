af779364ef1662ea6a6a53840219e6cebe84bc45
nice0001-angelo <nice0001@gmail.com>
2021-01-07 04:20:51 -0500

    update

---
+++ src/main/java/com/jin/algorithm/stage1/FindMax.java
@@ -0,0 +1,19 @@
+package com.jin.algorithm.stage1;
+
+import java.util.*;
+
+public class FindMax {
+
+	public static void main(String[] args) {
+		Scanner StdIn = new Scanner(System.in);
+		
+		System.out.println("Input 1st Number; \n");
+		int number1 = StdIn.nextInt();
+		System.out.println("Input 2nd Number; \n");
+		int number2 = StdIn.nextInt();
+		System.out.println("Input 3rd Number; \n");
+		int number3 = StdIn.nextInt();
+
+	}
+
+}
