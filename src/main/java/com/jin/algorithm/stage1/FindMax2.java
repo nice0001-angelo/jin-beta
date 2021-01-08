02b8fe95d576c93511c2131130a06e941b2fec49
nice0001-angelo <nice0001@gmail.com>
2021-01-07 21:37:53 -0500

    add

---
+++ src/main/java/com/jin/algorithm/stage1/FindMax2.java
@@ -0,0 +1,15 @@
+package com.jin.algorithm.stage1;
+
+public class FindMax2 {
+
+	public static void main(String[] args) {
+		int max = 0;
+		System.out.println("input the number: \n");
+
+	}
+	
+	
+	private static int findMax2(int a, int b, int c) {
+		return 0;
+	}
+}
