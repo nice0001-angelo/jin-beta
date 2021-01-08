5bd89de82985e944bafddcb61b4cdf73cac76ee5
nice0001-angelo <nice0001@gmail.com>
2021-01-08 00:02:41 -0500

    update

--- src/main/java/com/jin/algorithm/stage1/FindMax2.java
+++ src/main/java/com/jin/algorithm/stage1/FindMax2.java
@@ -1,11 +1,22 @@
 package com.jin.algorithm.stage1;
 
+import java.util.*;
+
 public class FindMax2 {
 
 	public static void main(String[] args) {
 		int max = 0;
-		System.out.println("input the number: \n");
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("input the number 1: \n");
+		int a = stdIn.nextInt();
+		System.out.println("input the number 2: \n");
+		int b = stdIn.nextInt();
+		System.out.println("Input the number 2: \n");
+		int c = stdIn.nextInt();
 
+		max = findMax2(a, b, c);
+		System.out.println("value: "+max);
 	}
 	
 	
