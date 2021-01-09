5b2913efc74c6af0ccb8ecc0c5ea118548b23504
nice0001-angelo <nice0001@gmail.com>
2021-01-09 06:27:28 -0500

    update

--- src/main/java/com/jin/algorithm/stage1/Findmax3.java
+++ src/main/java/com/jin/algorithm/stage1/Findmax3.java
@@ -5,11 +5,14 @@ import java.util.*;
 public class Findmax3 {
 
 	public static void main(String[] args) {
-		int[] max = new int[5];
+		int max = 0; 
+		int[] input = new int[5];
+		
 		
 		Scanner stdIn = new Scanner(System.in);
-		System.out.println("Insert number1: ");
 		
+		System.out.println("Insert number1: ");
+		input[0] = stdIn.nextInt();
 	}
 
 }
