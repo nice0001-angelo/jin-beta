99eeb36d024f37bf6bbf6097daf617cf0553dec0
nice0001-angelo <nice0001@gmail.com>
2021-01-09 06:22:51 -0500

    update

--- src/main/java/com/jin/algorithm/stage1/Findmax3.java
+++ src/main/java/com/jin/algorithm/stage1/Findmax3.java
@@ -1,10 +1,15 @@
 package com.jin.algorithm.stage1;
 
+import java.util.*;
+
 public class Findmax3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] max = new int[5];
+		
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("Insert number1: ");
+		
 	}
 
 }
