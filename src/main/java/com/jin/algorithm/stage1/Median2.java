3fcc38c08b42d87e4fd04bdc5f59a81e2f1024e9
nice0001-angelo <nice0001@gmail.com>
2021-01-11 00:44:08 -0500

    update

--- src/main/java/com/jin/algorithm/stage1/Median2.java
+++ src/main/java/com/jin/algorithm/stage1/Median2.java
@@ -6,13 +6,20 @@ public class Median2 {
 
 	public static void main(String[] args) {
 		
+		int result = 0;
 		Scanner stdIn = new Scanner(System.in);
 		
 		System.out.println("Input 3 number: ");
 		int a = stdIn.nextInt();
 		int b = stdIn.nextInt();
 		int c = stdIn.nextInt();
+		
+		result = median2(a, b, c);
+	}
 
+	private static int median2(int a, int b, int c) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
