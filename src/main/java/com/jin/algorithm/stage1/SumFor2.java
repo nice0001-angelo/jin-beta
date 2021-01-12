935687f51ef0d9a78ee20436a01c8da4b5c51583
nice0001-angelo <nice0001@gmail.com>
2021-01-12 05:16:39 -0500

    update

--- src/main/java/com/jin/algorithm/stage1/SumFor2.java
+++ src/main/java/com/jin/algorithm/stage1/SumFor2.java
@@ -5,9 +5,20 @@ import java.util.*;
 public class SumFor2 {
 
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
-		int input = stdIn.nextInt();
-
+		try {
+			Scanner stdIn = new Scanner(System.in);
+			int input = stdIn.nextInt();
+			int result = sumFor2(input);
+			System.out.println();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 	}
 
+	private static int sumFor2(int input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
+
