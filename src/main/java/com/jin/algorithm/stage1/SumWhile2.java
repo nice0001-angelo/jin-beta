2a9d64a32db07efe74b75303bcceb0a7907b4026
nice0001-angelo <nice0001@gmail.com>
2021-01-13 07:38:46 -0500

    update

--- src/main/java/com/jin/algorithm/stage1/SumWhile2.java
+++ src/main/java/com/jin/algorithm/stage1/SumWhile2.java
@@ -1,9 +1,18 @@
 package com.jin.algorithm.stage1;
 
+import java.util.*;
+
 public class SumWhile2 {
 
 	public static void main(String[] args) {
 		int result = 0;
+		Scanner stdIn = new Scanner(System.in);
+		try {
+			System.out.println("Input n value: ");
+			int input = stdIn.nextInt(); 
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
 	}
 
