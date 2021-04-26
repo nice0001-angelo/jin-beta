3d076b29c3ae5dfc4a7c8c03fcc0fea9099eb2f4
nice0001-angelo <nice0001@gmail.com>
2021-04-26 13:31:07 -0400

    update

--- src/main/java/com/jin/algorithm/stage1/FindMax1.java
+++ src/main/java/com/jin/algorithm/stage1/FindMax1.java
@@ -5,28 +5,35 @@ import java.util.*;
 public class FindMax1 {
 
 	public static void main(String[] args) {
-		Scanner StdIn = new Scanner(System.in);
-		
-		System.out.println("Input 1st Number; \n");
-		int number1 = StdIn.nextInt();
-		System.out.println("Input 2nd Number; \n");
-		int number2 = StdIn.nextInt();
-		System.out.println("Input 3rd Number; \n");
-		int number3 = StdIn.nextInt();
-		
-		int result = findMax1(number1, number2, number3);
-		System.out.println("Max is "+result);
-		
-	}
 
-	private static int findMax1(int a, int b, int c) {
-		int result = a;
-		if(b > result) {
-			result = b;
-		} else if(c > result) {
-			result = c;
+		
+		String yes = "Jimmy";
+		
+		String[] strArray = yes.split("");
+		
+		System.out.println(strArray);
+		
+		for(int i=0; i<strArray.length; i++) {
+			System.out.println("The result of strArray is: "+strArray[i]);
 		}
-		return result;
+
+		/* Scanner StdIn = new Scanner(System.in); */
+		
+		/*
+		 * System.out.println("Input 1st Number; \n"); int number1 = StdIn.nextInt();
+		 * System.out.println("Input 2nd Number; \n"); int number2 = StdIn.nextInt();
+		 * System.out.println("Input 3rd Number; \n"); int number3 = StdIn.nextInt();
+		 * 
+		 * int result = findMax1(number1, number2, number3);
+		 * System.out.println("Max is "+result);
+		 */
+	
+		
+		
 	}
 
+	/*
+	 * private static int findMax1(int a, int b, int c) { int result = a; if(b >
+	 * result) { result = b; } else if(c > result) { result = c; } return result; }
+	 */
 }
