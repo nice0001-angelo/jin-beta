3b1428b0579b1f9cbe668c706933ecdfe06ec037
nice0001-angelo <nice0001@gmail.com>
2021-05-03 03:56:28 -0400

    update

--- src/main/java/com/jin/amazon/arraysnstrings/TwoSum.java
+++ src/main/java/com/jin/amazon/arraysnstrings/TwoSum.java
@@ -18,7 +18,8 @@ public class TwoSum {
 		// TODO Auto-generated method stub
 		int[] nums = {2,7,11,15};
 		int target = 9;
-		int[] result = twoSum(nums, target);
+		//int[] result = twoSum(nums, target);
+		int[] result = twoSumHashed2(nums, target);
 		System.out.println("the result is: "+(String)result.toString());
 
 	}
@@ -41,7 +42,12 @@ public class TwoSum {
     	}
     	for(int i = 0; i < nums.length; i++) {
     		int complement = target - nums[i];
+    		if(map.containsKey(complement) && map.get(complement) != i) {
+    			return new int[] {i, map.get(complement)};
+    		}
     	}
+    	throw new IllegalArgumentException("Cant slove problem ");
     }
+    
 }
 
