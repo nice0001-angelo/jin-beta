a200d748b6b3e1582d30756169f18bd37d633e20
nice0001-angelo <nice0001@gmail.com>
2021-05-03 05:08:21 -0400

    update

--- src/main/java/com/jin/amazon/arraysnstrings/LongestSubstring.java
+++ src/main/java/com/jin/amazon/arraysnstrings/LongestSubstring.java
@@ -15,13 +15,19 @@ public class LongestSubstring {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String s = "abcabcbb";
+		
+		lengthOfLongestSubstring(s);
+		
 	}
 
 	public static int lengthOfLongestSubstring(String s) {
+		String[] strArray = s.split("");
+		System.out.println("strArray: "+strArray);
+		System.out.println("strArray size: "+strArray.length);
+		for(int i = 0; i< strArray.length; i++) {
+			
+		}
 		
-		
-		
-		
-		return ;
 	}
 }
