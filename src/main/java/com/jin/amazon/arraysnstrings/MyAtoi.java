02a524d4f61654ff099dc633de23eddb1958318d
nice0001-angelo <nice0001@gmail.com>
2021-05-03 06:46:16 -0400

    add

---
+++ src/main/java/com/jin/amazon/arraysnstrings/MyAtoi.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.amazon.arraysnstrings;
+
+/**
+ * @author njh
+ *
+ */
+public class MyAtoi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
