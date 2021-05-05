120faf915d05f2b0dd589f45f5b01fafd53bc6cb
nice0001-angelo <nice0001@gmail.com>
2021-05-05 10:36:27 -0400

    update

--- src/main/java/com/jin/amazon/arraysnstrings/IToRom.java
+++ src/main/java/com/jin/amazon/arraysnstrings/IToRom.java
@@ -12,12 +12,21 @@ public class IToRom {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	private static final String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
+	
+	public static void main(String[] args) {
+
+		
+		
 	}
 
-	public static String integerToRoman() {
+	
+	public static String integerToRoman(int num) {
+		StringBuilder sb = new StringBuilder();
 		
+		//Loop through each symbol, stopping if num becomes 0.
+		for(int i = 0; i < values.length ; )
 	}
 }
