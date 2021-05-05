4b1d84d3b6ad4ff092176816d7bac4a8c8e2b5c7
nice0001-angelo <nice0001@gmail.com>
2021-05-05 03:45:38 -0400

    update

--- src/main/java/com/jin/amazon/arraysnstrings/MostWater.java
+++ src/main/java/com/jin/amazon/arraysnstrings/MostWater.java
@@ -13,20 +13,25 @@ public class MostWater {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//int[] height = {7,6,5,3,5,6};
+		int[] height = {1,8,6,2,5,4,8,3,7};
+		int output = maxArea(height);
+		System.out.println("the result is: "+output);
 
 	}
 	
 	public static int maxArea(int[] height) {
-		int result = ;
+		int result = 0;
 		
 		for(int i = 0 ; i < height.length ; i++) {
 			for(int j = i+1 ; j < height.length ; j++) {
-				result = (height[i]*height[j]);
-				
+				int smt = (height[i]*height[j]);
+				if(smt >= result)
+				result = smt;
 			}
-				
+
 		}
+		return result;
 	}
 
 }
