d03a6d2ebdb7fa6363cec6e61042aafde4534157
nice0001-angelo <nice0001@gmail.com>
2021-05-06 07:33:33 -0400

    update

--- src/main/java/com/jin/amazon/arraysnstrings/ThreeSum.java
+++ src/main/java/com/jin/amazon/arraysnstrings/ThreeSum.java
@@ -14,11 +14,20 @@ public class ThreeSum {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] input = {-1,0,1,2,-1,-4};
+		int result = threeSum(input);
+		System.out.println("result: "+result);
 
 	}
 
 	
 	public static int threeSum(int[] nums) {
-		
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = i+1; j< nums.length; j++) {
+				for(int k = j+1; k < nums.length; k++) {
+					
+				}
+			}
+		}
 	}
 }
