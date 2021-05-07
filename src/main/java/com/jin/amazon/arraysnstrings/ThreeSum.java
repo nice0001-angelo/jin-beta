8a59f0a8d673f41125cff0b5e126d2a667cd5fc3
nice0001-angelo <nice0001@gmail.com>
2021-05-07 09:54:46 -0400

    update

--- src/main/java/com/jin/amazon/arraysnstrings/ThreeSum.java
+++ src/main/java/com/jin/amazon/arraysnstrings/ThreeSum.java
@@ -21,11 +21,13 @@ public class ThreeSum {
 	}
 
 	
-	public static int threeSum(int[] nums) {
+	public static List<List<Integer>> threeSum(int[] nums) {
 		for(int i = 0; i < nums.length; i++) {
 			for(int j = i+1; j< nums.length; j++) {
 				for(int k = j+1; k < nums.length; k++) {
-					
+					if(nums[i]+nums[j]+nums[k] == 0) {
+					System.out.println("["+nums[i]+","+nums[j]+","+nums[k]+"]");
+					}
 				}
 			}
 		}
