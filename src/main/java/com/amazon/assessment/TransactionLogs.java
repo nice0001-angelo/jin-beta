dbe91a3f0cbb2c53d5e8619a5737b670ba00e502
nice0001-angelo <nice0001@gmail.com>
2021-05-09 13:51:51 -0400

    update

--- src/main/java/com/amazon/assessment/TransactionLogs.java
+++ src/main/java/com/amazon/assessment/TransactionLogs.java
@@ -27,7 +27,52 @@ public class TransactionLogs {
 		
 		for(String log : logs) {
 			String[] logArray = log.split(" ");
+			String fromId = logArray[0];
+			String toId = logArray[1];
+			if(fromId.equals(toId)) {
+				if(null == chkMap.get(fromId)) {
+					chkMap.put(fromId, 1);
+				} else {
+					chkMap.put(fromId, chkMap.get(fromId)+1);
+				}
+			} else {
+				if(null == chkMap.get(fromId)) {
+					chkMap.put(fromId,1);
+				} else {
+					chkMap.put(fromId, chkMap.get(fromId)+1);
+				}
+				if (null == chkMap.get(toId)) {
+					chkMap.put(toId, 1);
+				} else {
+					chkMap.put(toId, chkMap.get(toId)+1);
+				}
+			}
 			
+		}
+		
+		int[][] chkInt = new int[chkMap.size()][2];
+		
+		int i = 0;
+		for(String id : chkMap.keySet()) {
+			chkInt[i][0] = Integer.parseInt(id);
+			chkInt[i][1] = chkMap.get(id);
+			i++;
+		}
+		
+		Arrays.sort(chkInt, 
+				new Comparator<int[]>() {
+					public int compare(final int[] a, final int[] b) {
+						return a[0] - b[0];
+					}
+				});
+		
+		for(int j =0; j < chkInt.length; j++) {
+			if(chkInt[j][1] >= threshold) {
+				resultList.add(String.valueOf(chkInt[j][0]));
+			}
+		}
+		if(resultList.size() == 0) {
+			resultList.add("");
 		}
 	}
 
