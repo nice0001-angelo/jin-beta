7b5ed85c700732e8c343a4db8daf3bc45d8d6898
nice0001-angelo <nice0001@gmail.com>
2021-05-09 04:28:44 -0400

    update

--- src/main/java/com/jin/amazon/arraysnstrings/RotateImage.java
+++ src/main/java/com/jin/amazon/arraysnstrings/RotateImage.java
@@ -14,17 +14,19 @@ public class RotateImage {
 	 */
 	public static void main(String[] args) {
 		int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-		int[][] result = rotateImage(matrix);
-		System.out.println("result: "+result);
+		//int[][] result = rotateImage(matrix);
+		//System.out.println("result: "+result);
+		System.out.println(matrix[0][2]);
 	}
 	
-	public static int[][] rotateImage(int[][] matrix){
-		int n = matrix.length;
-		for(int i = 0; i < (n+1)/2; i++) {
-			for(int j=0; j< n/2; j++) {
-				
-			}
-		}
-	}
+//	public static int[][] rotateImage(int[][] matrix){
+//		int n = matrix.length;
+//		for(int i = 0; i < (n+1)/2; i++) {
+//			for(int j=0; j< n/2; j++) {
+//				int temp = matrix[n-1-j][i];
+//				
+//			}
+//		}
+//	}
 
 }
