39c3516bc0eecab1c4a52eb4fd7ceb190223091d
nice0001-angelo <nice0001@gmail.com>
2021-05-09 14:30:28 -0400

    update

--- src/main/java/com/amazon/assessment/RobotRodeo.java
+++ src/main/java/com/amazon/assessment/RobotRodeo.java
@@ -3,6 +3,8 @@
  */
 package com.amazon.assessment;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -17,4 +19,29 @@ public class RobotRodeo {
 
 	}
 
+	public static List<String> doesCircleExist(List<String> commands){
+		int[] dx = {-1,0,1,0};
+		int[] dy = {0,1,0,-1};
+		int x, y, dir;
+		
+		List<String> resultList = new ArrayList<String>();
+		
+		for(String command : commands) {
+			String result = "";
+			x=0;
+			y=0;
+			dir=0;
+			for(char comm : command.toCharArray()) {
+				if(comm == 'G') {
+					x += dx[dir];
+					y += dy[dir];
+					} else if (comm == 'L') {
+						dir = (dir+1) % 4;
+					} else {
+						dir = (dir+3) % 4;
+					}
+				}
+			}
+		}
+	}
 }
