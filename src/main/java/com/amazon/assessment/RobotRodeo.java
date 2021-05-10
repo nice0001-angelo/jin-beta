b00da01c76b02bb1c4e95b50c1eb64f6fa08440a
nice0001-angelo <nice0001@gmail.com>
2021-05-10 10:11:07 -0400

    update

--- src/main/java/com/amazon/assessment/RobotRodeo.java
+++ src/main/java/com/amazon/assessment/RobotRodeo.java
@@ -7,7 +7,45 @@ import java.util.*;
 
 /**
  * @author njh
- *
+ * the Amazon robotics team wants to demonstrate how easy it is to use their latest robot.
+ * For the event, they have built a simple language to control it:
+ * G instructs the robot to move forward one step.
+ * L instructs the robot to turn left in place.
+ * R instructs the robot to turn right in place.
+ * 
+ * To keep the event more interactive, once the robot has completed the list of instructions,
+ * it will repeat them in an infinite loop.
+ * Given the time and effort in building this robot, the robotics team don't want to lose it 
+ * because of a bad sequence of instructions.
+ * They'veasked you to build a simulator based on a list of commands that will determine
+ * if it exists a circle such that the robot always moves with the circle.
+ * 
+ * Consider the commands R and G executed infinitely.
+ * A diagram of the robot's movement looks like:
+ * RG -> RG
+ * ^
+ * |     | 
+ *       V 
+ * RG <- RG
+ * 
+ * The robot will never leave the circle.
+ * 
+ * Function Description
+ * Complete the function doesCircleExist in the editor below.
+ * The function must return an array of n strings either YES or NO 
+ * based on whether the robot is bound within a circle or not, in order of test results
+ * 
+ *  doesCircleExist has the following parameter(s):
+ *  commands[commands[0],...commands[n-1]]: As array of n commands[i]
+ *  where eache represents a list of commands to test.
+ *  
+ *  Constraints
+ *  1<=|commands[i]|<=2500
+ *  1<=n<=10
+ *  Each command consists of G, L, and R only
+ * 
+ * 
+ * 
  */
 public class RobotRodeo {
 
@@ -16,8 +54,8 @@ public class RobotRodeo {
 	 */
 	public static void main(String[] args) {
 		List<String> commands = new ArrayList<>(); 
-		commands.add(0,"R");
-		commands.add(1,"G");
+		commands.add(0,"RG");
+		//commands.add(1,"G");
 		//System.out.println(commands.get(1));
 		List<String> result = new ArrayList<>();
 		result = doesCircleExist(commands);
