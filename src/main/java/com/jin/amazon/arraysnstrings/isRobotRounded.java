444af2f9884337fd75ecd66bddb3d82949eadcf5
nice0001-angelo <nice0001@gmail.com>
2021-05-13 04:09:07 -0400

    update

--- src/main/java/com/jin/amazon/arraysnstrings/isRobotRounded.java
+++ src/main/java/com/jin/amazon/arraysnstrings/isRobotRounded.java
@@ -19,6 +19,7 @@ public class isRobotRounded {
 
 	
 	public static boolean isRobotRounded{
-		
+		// north = 0, east = 1, south = 2, west = 3
+		int[][] directions = new int[][] {{0,1},{1,0},{0,-1},{-1,0}};
 	}
 }
