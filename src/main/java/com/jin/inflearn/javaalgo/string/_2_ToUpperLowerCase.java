0c98eeaa85fb326d9392c3610147f64cf5f0d080
nice0001-angelo <nice0001@gmail.com>
2021-05-14 02:39:44 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_2_ToUpperLowerCase.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_2_ToUpperLowerCase.java
@@ -26,11 +26,11 @@ public class _2_ToUpperLowerCase {
 		String answer = "";
 		for(char x : str.toCharArray()) {
 			//if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
-			
-			//대문자 ASCII 65~90, 소문자 ASCII 97~122
+			//대문자 ASCII 65~90, 소문자 ASCII 97~122 (소문자에서 32를 빼면 대문자가 됨)
+			if(x>=97 && x<=122) answer+=(x-32);
 			
 			//else answer += Character.toLowerCase(x);
-			
+			else answer+=Character.toUpperCase(x);
 		}
 		return answer;
 	}
