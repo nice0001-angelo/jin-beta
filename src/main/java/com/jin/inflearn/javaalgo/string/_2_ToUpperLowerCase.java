2095bbb81278f48e8b45e792e71b5780a6646fc2
nice0001-angelo <nice0001@gmail.com>
2021-05-14 01:02:23 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgo/string/_2_ToUpperLowerCase.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgo.string;
+
+/**
+ * @author njh
+ *
+ */
+public class _2_ToUpperLowerCase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		_2_ToUpperLowerCase T = new _2_ToUpperLowerCase();
+
+		T.solution
+	}
+
+	public String solution() {
+		String result;
+		return result;
+	}
+}
