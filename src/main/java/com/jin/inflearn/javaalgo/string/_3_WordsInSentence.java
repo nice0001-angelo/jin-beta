27b484512ec774ac353841e1c6bac316a19f80b1
nice0001-angelo <nice0001@gmail.com>
2021-05-14 03:48:26 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_3_WordsInSentence.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_3_WordsInSentence.java
@@ -16,12 +16,20 @@ public class _3_WordsInSentence {
 	 */
 	public static void main(String[] args) {
 		_3_WordsInSentence T = new _3_WordsInSentence();
-		
+		Scanner kb = new Scanner(System.in);
+		String str = kb.nextLine();
+		System.out.println(T.solution(str));
 		
 	}
 	
 	public String solution(String str) {
-
+		String answer = "";
+		String[] s = str.split(str);
+		for(String x : s) {
+			System.out.println(x);
+			answer += x;
+		}
+		return answer;
 	}
 
 }
