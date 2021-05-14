ffc63e8ade520a9aebbffa41f760eae90f39bd2a
nice0001-angelo <nice0001@gmail.com>
2021-05-13 22:00:07 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_4_ReverseWords.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_4_ReverseWords.java
@@ -15,8 +15,14 @@ public class _4_ReverseWords {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Main T = new Main();
+		
+		//static 에서 instance(Non-static) 객체를 생성하기 위해서는 이렇게 클래스 객체를 생성해야함
+		//이렇게 하면 호출할 메서드를 static으로 선언하지 않아도 됨
+		_4_ReverseWords T = new _4_ReverseWords();
+		
 		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		char c = kb.next().charAt(0);//String을 읽어들이고(next()) charAt(0)을 통해서 첫번째 char 값을 가져오는 것임
 		
 		System.out.println();
 
