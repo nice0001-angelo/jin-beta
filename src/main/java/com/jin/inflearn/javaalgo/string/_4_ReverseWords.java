67bab0ad54f462e8c93f17c2f8de77af8d42ecc5
nice0001-angelo <nice0001@gmail.com>
2021-05-15 05:27:43 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_4_ReverseWords.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_4_ReverseWords.java
@@ -3,6 +3,8 @@
  */
 package com.jin.inflearn.javaalgo.string;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -14,7 +16,13 @@ public class _4_ReverseWords {
 	 */
 	public static void main(String[] args) {
 		_4_ReverseWords T = new _4_ReverseWords();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
 
 	}
 
+	public void solution(String str) {
+		
+	}
 }
