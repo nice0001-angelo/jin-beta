28d1018e83eedfd02d762afd6e5189b30c7affab
nice0001-angelo <nice0001@gmail.com>
2021-05-16 12:20:48 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgo/string/_9_ExtractNum.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgo.string;
+
+import java.util.*;
+
+/**
+ * @author njh
+ *
+ */
+public class _9_ExtractNum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		_9_ExtractNum T = new _9_ExtractNum();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.nextLine();
+		System.out.println(T.solution(str));
+	}
+
+	public String solution(String str) {
+		
+		return "";
+	}
+
+}
