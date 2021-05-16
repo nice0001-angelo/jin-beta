7b63e93d321d42c5228987156148ee072a9ef974
nice0001-angelo <nice0001@gmail.com>
2021-05-16 13:12:18 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_9_ExtractNum.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_9_ExtractNum.java
@@ -17,13 +17,16 @@ public class _9_ExtractNum {
 	public static void main(String[] args) {
 		_9_ExtractNum T = new _9_ExtractNum();
 		Scanner kb = new Scanner(System.in);
-		String str = kb.nextLine();
+		String str = kb.next();
 		System.out.println(T.solution(str));
 	}
 
-	public String solution(String str) {
-		
-		return "";
+	public int solution(String str) {
+		int answer = 0;
+		for(char x : str.toCharArray()) {
+			if(x >=48 && x<=57) answer = answer*10 + (x-48); //숫자 0~9: ASCII 48~57 
+		}
+		return answer;
 	}
 
 }
