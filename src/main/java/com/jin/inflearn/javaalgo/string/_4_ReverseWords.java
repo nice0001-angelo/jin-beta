a746760b43abe206c8eafcd42d968f463e24c562
nice0001-angelo <nice0001@gmail.com>
2021-05-16 04:42:29 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_4_ReverseWords.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_4_ReverseWords.java
@@ -17,39 +17,30 @@ public class _4_ReverseWords {
 	public static void main(String[] args) {
 		_4_ReverseWords T = new _4_ReverseWords();
 		Scanner kb = new Scanner(System.in);
-		int n = kb.nextInt();
-		String[] str = new String[n];
-		for(int i = 0; i < n; i++) {
+		int n = kb.nextInt(); // 숫자 1자리수 받기
+		String[] str = new String[n]; // 스트링 배역 갯수를 n개로 제한
+		// str[] 에 각각의 문자를 배열로 저장
+		for (int i = 0; i < n; i++) {
 			str[i] = kb.next();
 		}
+
+		
+		 //solution을 호출하고 각각의 배열 값을 하나씩 print 함 
 		for(String x: T.solution(n, str)) {
-		System.out.println(x);
-		}
+		 System.out.println(x); 
+		 }
+		 
+
+		// System.out.println(T.solution(n, str));
 	}
-	
-	
+
 	public List<String> solution(int n, String[] str) {
 		List<String> answer = new ArrayList<>();
-		/* StringBuilde를 이용한 리버스
-		 * for(String x : str) { 
-		 * String tmp = new StringBuilder(x).reverse().toString();
-		 * answer.add(tmp); 
-		 * }
+		/*
+		 * StringBuilde를 이용한 리버스 for(String x : str) { String tmp = new
+		 * StringBuilder(x).reverse().toString(); answer.add(tmp); }
 		 */
-		for(String x : str) {
-			char[] s = x.toCharArray();
-			int lt = 0;
-			int rt = x.length()-1;
-			while(rt > lt) {
-				char tmp = s[lt];
-				s[lt] = s[rt];
-				s[rt] = tmp;
-				rt--;
-				lt++;
-			}
-			String tmp = String.valueOf(s);
-			answer.add(tmp);
-		}
+
 		return answer;
 	}
 }
