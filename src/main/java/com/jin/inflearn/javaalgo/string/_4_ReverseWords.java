2fe9766926aa524a85772abb7221588325b75b0b
nice0001-angelo <nice0001@gmail.com>
2021-05-16 03:22:29 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_4_ReverseWords.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_4_ReverseWords.java
@@ -31,9 +31,14 @@ public class _4_ReverseWords {
 	public List<String> solution(int n, String[] str) {
 		List<String> answer = new ArrayList<>();
 		/* StringBuilde를 이용한 리버스
-		 * for(String x : str) { String tmp = new StringBuilder(x).reverse().toString();
-		 * answer.add(tmp); }
+		 * for(String x : str) { 
+		 * String tmp = new StringBuilder(x).reverse().toString();
+		 * answer.add(tmp); 
+		 * }
 		 */
+		for(String x : str) {
+			char[] s = x.toCharArray();
+		}
 		return answer;
 	}
 }
