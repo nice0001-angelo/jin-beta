7677abbadaf95f78ceb4aad8304d80fa1117c03b
nice0001-angelo <nice0001@gmail.com>
2021-05-16 06:51:36 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgo/string/_6_DeleteDupChar.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgo.string;
+
+import java.util.*;
+
+/**
+ * @author njh
+ *
+ */
+public class _6_DeleteDupChar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		_6_DeleteDupChar T = new _6_DeleteDupChar();
+		Scanner tb = new Scanner(System.in);
+		String str = tb.next();
+		System.out.println(T.solution(str));
+	}
+
+	public String solution(String str) {
+		String answer = new String();
+		return answer;
+	}
+}
