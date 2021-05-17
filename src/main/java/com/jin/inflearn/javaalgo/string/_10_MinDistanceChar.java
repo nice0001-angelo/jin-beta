526cebf1836122a7504b50d60f3c712b2c81034c
nice0001-angelo <nice0001@gmail.com>
2021-05-17 10:45:41 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_10_MinDistanceChar.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_10_MinDistanceChar.java
@@ -19,10 +19,24 @@ public class _10_MinDistanceChar {
 		Scanner kb = new Scanner(System.in);
 		String str = kb.next();
 		char t = kb.next().charAt(0);
+		for(int x : T.solution(str, t)) {
+			System.out.println(x+" ");
+		}
 	}
 	
-	public int solution(String str, char t) {
-		return 0;
+	public int[] solution(String str, char t) {
+		int[] answer = new int[str.length()];
+		int p = 1000;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == t) {
+				p = 0;
+				answer[i] = p;
+			} else {
+				p++;
+				answer[i] = p;
+			}
+		}
+		return answer;
 	}
 	
 	
