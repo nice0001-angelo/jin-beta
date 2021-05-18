d4be40afc606f4774526e1cc3269fc7bb6e67543
nice0001-angelo <nice0001@gmail.com>
2021-05-17 22:19:03 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_11_CompressSentence.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_11_CompressSentence.java
@@ -22,14 +22,14 @@ public class _11_CompressSentence {
 	}
 
 	public String solution(String str) {
-		int p = 0;
+		int cnt = 1;
 		String result = new String();
 		for(int i = 0; i < str.length()-1; i++) {
 			for(int j = i+1; j < str.length(); j++) {
 				if(str.charAt(i) == str.charAt(j)) {
-					p++;
+					cnt++;
 					result = Character.toString(str.charAt(i));
-					result += p; 
+					result += cnt; 
 				}
 			}
 		}
