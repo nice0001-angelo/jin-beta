3f03601cf1e16672948321ec9e64c1a956840635
nice0001-angelo <nice0001@gmail.com>
2021-05-18 00:20:35 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgo/string/_1_12_Decription.java
+++ src/main/java/com/jin/inflearn/javaalgo/string/_1_12_Decription.java
@@ -3,6 +3,8 @@
  */
 package com.jin.inflearn.javaalgo.string;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -13,8 +15,13 @@ public class _1_12_Decription {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		_1_12_Decription T = new _1_12_Decription();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
 	}
 
+	public String solution(String str) {
+		return "";
+	}
 }
