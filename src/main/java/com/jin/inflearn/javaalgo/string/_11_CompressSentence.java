b776e8810c34bde46313519c2f8b8b109beed5f9
nice0001-angelo <nice0001@gmail.com>
2021-05-17 21:42:32 -0400

    UPDATE

---
+++ src/main/java/com/jin/inflearn/javaalgo/string/_11_CompressSentence.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgo.string;
+
+import java.util.*;
+
+/**
+ * @author njh
+ *
+ */
+public class _11_CompressSentence {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		_11_CompressSentence T = new _11_CompressSentence();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
+	}
+
+	public
+}
