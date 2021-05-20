e3cc4cda0cca55acf41fcdbb1ae73b18a2479394
nice0001-angelo <nice0001@gmail.com>
2021-05-20 03:08:21 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_2_VisibleStudent.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_2_VisibleStudent.java
@@ -13,7 +13,7 @@ public class _2_VisibleStudent {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		_2_VisibleStudent T = new _2_VisibleStudent();
 
 	}
 
