f2a12a0daaf0c38496b6f650bb04a9cbf2645fec
nice0001-angelo <nice0001@gmail.com>
2021-05-24 11:23:31 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_6_ReversePrimeNumber.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.array;
+
+import java.util.*;
+
+/**
+ * @author njh
+ *
+ */
+public class _6_ReversePrimeNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		_6_ReversePrimeNumber T = new _6_ReversePrimeNumber();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+
+	}
+
+}
