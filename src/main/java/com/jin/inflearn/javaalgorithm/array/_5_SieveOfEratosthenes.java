0be8ccc2104d472593f7645aebfb5e64dafd6b14
nice0001-angelo <nice0001@gmail.com>
2021-05-24 00:21:21 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_5_SieveOfEratosthenes.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_5_SieveOfEratosthenes.java
@@ -3,6 +3,8 @@
  */
 package com.jin.inflearn.javaalgorithm.array;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -13,8 +15,15 @@ public class _5_SieveOfEratosthenes {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		_5_SieveOfEratosthenes T = new _5_SieveOfEratosthenes();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		System.out.println(T.solution(n));
+	}
+	
+	public int solution(int n) {
+		int answer = 0;
+		return answer;
 	}
 
 }
