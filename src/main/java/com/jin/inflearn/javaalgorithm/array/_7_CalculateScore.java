41a41ea420215726b92adcb4b0aa31020965e541
nice0001-angelo <nice0001@gmail.com>
2021-05-24 14:04:28 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_7_CalculateScore.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.array;
+
+/**
+ * @author njh
+ *
+ */
+public class _7_CalculateScore {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		_7_CalculateScore T = new _7_CalculateScore();
+
+	}
+
+}
