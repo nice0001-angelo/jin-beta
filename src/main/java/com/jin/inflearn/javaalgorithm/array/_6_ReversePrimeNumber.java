eff4edfe6cf30d9af87e9d6788f050ac44fe98f2
nice0001-angelo <nice0001@gmail.com>
2021-05-24 13:33:04 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_6_ReversePrimeNumber.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_6_ReversePrimeNumber.java
@@ -22,25 +22,40 @@ public class _6_ReversePrimeNumber {
 		for(int i = 0; i < n; i++) {
 			arr[i] = kb.nextInt();
 		}
-
-		System.out.println(T.solution(n, arr));
+		for(int x : T.solution(n, arr)) {
+			System.out.print(x+" ");
+		}
+		
 		
 	}
 	
-	public ArrayList<String> solution(int n, int[] arr) {
-		ArrayList<String> answer = new ArrayList<String>();
+	public ArrayList<Integer> solution(int n, int[] arr) {
+		ArrayList<Integer> answer = new ArrayList<Integer>();
 		for(int i = 0; i < n; i++) {
-			int tmp = arr[i];
+			int tmp = arr[i]; //25
 			int res = 0;
 			while(tmp>0) {
 				int t = tmp%10; //arr[0] 25이면 5
 				res=res*10+t; //0*10+5=5
 				tmp=tmp/10; // 25/10=2.5
 			}
+			if(isPrime(res)) {
+				answer.add(res);
+			}
 		}
-		
-		
 		return answer;
+	}
+	
+	public boolean isPrime(int num) {
+		if(num==1) {
+			return false;
+		}
+		for(int i=2; i<num; i++) {
+			if(num%i==0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
