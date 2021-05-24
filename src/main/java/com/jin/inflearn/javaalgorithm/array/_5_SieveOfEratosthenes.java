6a13c4474ea24d55d186a886a5c56118a9468c0a
nice0001-angelo <nice0001@gmail.com>
2021-05-24 04:25:28 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_5_SieveOfEratosthenes.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_5_SieveOfEratosthenes.java
@@ -23,6 +23,16 @@ public class _5_SieveOfEratosthenes {
 	
 	public int solution(int n) {
 		int answer = 0;
+		int[] ch = new int[n+1];
+		for(int i = 2; i<=n; i++) {
+			if(ch[i] == 0) {
+				answer++;
+				for(int j=i; j<=n; j++) {
+					
+				}
+			}
+		}
+		
 		return answer;
 	}
 
