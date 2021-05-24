db21d5daccb6f98c60dbee5130205ea8aa54e2ab
nice0001-angelo <nice0001@gmail.com>
2021-05-24 00:02:38 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_5_SieveOfEratosthenes.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.array;
+
+/**
+ * @author njh
+ *
+ */
+public class _5_SieveOfEratosthenes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
