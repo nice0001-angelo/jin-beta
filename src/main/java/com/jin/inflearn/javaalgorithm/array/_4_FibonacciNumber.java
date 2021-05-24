50f338c113a134df82c27829ef43937bf92f7e35
nice0001-angelo <nice0001@gmail.com>
2021-05-23 22:31:11 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_4_FibonacciNumber.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.array;
+
+import java.util.*;
+
+/**
+ * @author njh
+ *
+ */
+public class _4_FibonacciNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		_4_FibonacciNumber T = new _4_FibonacciNumber();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		System.out.println(T.solution(n));
+	}
+
+	public String solution(int n) {
+		String answer = "";
+		
+		return answer;
+	}
+}
