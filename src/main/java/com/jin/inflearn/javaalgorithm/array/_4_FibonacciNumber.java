4fbb9da850ca933e9986bb12c2646d5c95b50daa
nice0001-angelo <nice0001@gmail.com>
2021-05-23 23:33:51 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_4_FibonacciNumber.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_4_FibonacciNumber.java
@@ -18,12 +18,19 @@ public class _4_FibonacciNumber {
 		_4_FibonacciNumber T = new _4_FibonacciNumber();
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
-		System.out.println(T.solution(n));
+		for(int x : T.solution(n)) {
+			System.out.print(x+" ");	
+		}
+		
 	}
 
-	public String solution(int n) {
-		String answer = "";
-		
+	public int[] solution(int n) {
+		int[] answer = new int[n];
+		answer[0] = 1;
+		answer[1] = 1;
+		for(int i = 2; i < n; i++) {
+			answer[i] = answer[i-1] + answer[i-2]; 
+		}
 		return answer;
 	}
 }
