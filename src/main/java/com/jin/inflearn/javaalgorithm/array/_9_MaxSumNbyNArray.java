f0588ed0aa82ccc38515b498e49a1e0b2ec9340b
nice0001-angelo <nice0001@gmail.com>
2021-05-25 04:14:52 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_9_MaxSumNbyNArray.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_9_MaxSumNbyNArray.java
@@ -13,6 +13,12 @@ public class _9_MaxSumNbyNArray {
 
 	/**
 	 * @param args
+5
+10 13 10 12 15
+12 39 30 23 11
+11 25 50 53 15
+19 27 29 37 27
+19 13 30 13 19 
 	 */
 	public static void main(String[] args) {
 		_9_MaxSumNbyNArray T = new _9_MaxSumNbyNArray();
@@ -30,6 +36,21 @@ public class _9_MaxSumNbyNArray {
 	
 	public int solution(int n, int[][] arr) {
 		int answer = Integer.MIN_VALUE;
+		int sum1, sum2;
+		for(int i=0; i<n; i++) {
+			sum1 = sum2 =0;
+			for(int j=0; j<n; j++) {
+				sum1 += arr[i][j];
+				sum2 += arr[j][i];
+			}
+			answer = Math.max(sum1, sum2);
+		}
+		sum1 = sum2 = 0;
+		for(int i=0; i<n; i++) {
+			sum1 += arr[i][i];
+			sum2 += arr[i][n-1-i];
+		}
+		answer = Math.max(sum1, sum2);
 		return answer;
 	}
 
