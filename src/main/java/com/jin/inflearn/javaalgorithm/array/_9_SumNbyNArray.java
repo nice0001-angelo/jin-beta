9465c54310f9bb6822c41c1e21e674c9dc273f97
nice0001-angelo <nice0001@gmail.com>
2021-05-25 03:16:40 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_9_SumNbyNArray.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_9_SumNbyNArray.java
@@ -3,6 +3,8 @@
  */
 package com.jin.inflearn.javaalgorithm.array;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -13,8 +15,22 @@ public class _9_SumNbyNArray {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		_9_SumNbyNArray T = new _9_SumNbyNArray();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[][] arr = new int[n][n];
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<n; j++) {
+				arr[i][j] = kb.nextInt();
+			}
+		}
+		System.out.println(T.solution(n, arr));
 
+	}
+	
+	public int solution(int n, int[][] arr) {
+		int answer = 0;
+		return answer;
 	}
 
 }
