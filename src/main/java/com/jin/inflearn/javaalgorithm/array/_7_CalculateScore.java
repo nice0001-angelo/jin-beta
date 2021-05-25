ca3d37180622677ade761c1aafcbb917144f859d
nice0001-angelo <nice0001@gmail.com>
2021-05-24 22:42:41 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_7_CalculateScore.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_7_CalculateScore.java
@@ -3,6 +3,8 @@
  */
 package com.jin.inflearn.javaalgorithm.array;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -14,7 +16,17 @@ public class _7_CalculateScore {
 	 */
 	public static void main(String[] args) {
 		_7_CalculateScore T = new _7_CalculateScore();
-
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+		for(int x : T.solution(n, arr)) {
+			
+		}
+		
+	}
+	
+	public int[] solution(int n, int[] arr) {
+		
 	}
 
 }
