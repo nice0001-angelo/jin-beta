be63d274e1952d8eaa31ad77eeb417c35452fc8f
nice0001-angelo <nice0001@gmail.com>
2021-05-25 01:14:27 -0400

    add

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_8_FindGrade.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.array;
+
+/**
+ * @author njh
+ *
+ */
+public class _8_FindGrade {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
