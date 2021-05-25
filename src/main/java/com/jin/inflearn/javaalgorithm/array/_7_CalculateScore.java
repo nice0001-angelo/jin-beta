413fe84f3e06ae833eb5721c9a6c10fede77ea7e
nice0001-angelo <nice0001@gmail.com>
2021-05-25 00:22:58 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_7_CalculateScore.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_7_CalculateScore.java
@@ -19,14 +19,29 @@ public class _7_CalculateScore {
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
 		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = kb.nextInt();
+		}
 		for(int x : T.solution(n, arr)) {
-			
+			System.out.print(x+" ");
 		}
 		
 	}
 	
 	public int[] solution(int n, int[] arr) {
-		
+		int[] answer = new int[n];
+		if(arr[0] == 1) {
+			answer[0] = 1;
+		} else {
+			answer[0] = 0;
+		}
+		for(int i = 1; i<n; i++) {
+			if(arr[i] == 1 ) {
+				answer[i] = answer[i-1]+1;
+			} else {
+				answer[i] = 0;
+			}
+		}
+		return answer;
 	}
-
 }
