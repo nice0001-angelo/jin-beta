84c720fed4c1c40bf6782f5f0700fe3ea328c07a
nice0001-angelo <nice0001@gmail.com>
2021-05-25 01:36:13 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_8_FindGrade.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_8_FindGrade.java
@@ -3,6 +3,8 @@
  */
 package com.jin.inflearn.javaalgorithm.array;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -13,8 +15,24 @@ public class _8_FindGrade {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		_8_FindGrade T = new _8_FindGrade();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		for(int x : T.solution(n, arr)) {
+			System.out.print(x+" ");
+		}
 	}
-
+	
+	public int[] solution(int n, int[] arr) {
+		int[] answer = new int[n];
+		Map<String, Integer> sortedArr = new HashMap<String, Integer>();
+		for(int i = 0; i<n; i++) {
+			
+		}
+		return answer;
+	}
 }
