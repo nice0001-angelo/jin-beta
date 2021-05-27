ab262b73aecfb7fc87f0bc402d9b94bbf3c8c4e7
nice0001-angelo <nice0001@gmail.com>
2021-05-27 04:27:25 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_10_FindBoss.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_10_FindBoss.java
@@ -13,13 +13,19 @@ public class _10_FindBoss {
 
 	/**
 	 * @param args
+5
+2 3 1 7 3
+4 1 9 6 8
+5 5 2 4 4
+6 5 2 6 7
+8 4 2 2 2
 	 */
 	public static void main(String[] args) {
 		_10_FindBoss T = new _10_FindBoss();
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
 		int[][] arr = new int[n+1][6];
-		for(int i=1; i<n; i++) {
+		for(int i=1; i<=n; i++) {
 			for(int j=1; j<=5; j++) {
 				arr[i][j]=kb.nextInt();
 			}
@@ -31,7 +37,21 @@ public class _10_FindBoss {
 	
 	public int solution(int n, int[][] arr) {
 		int answer=0, max=Integer.MAX_VALUE;
+		for(int i=1; i<=n; i++) {
+			int cnt=0;
+			for(int j=1; j<=n; j++) {
+				for(int k=1; k<=5; k++) {
+					if(arr[i][k]==arr[j][k]) {
+						cnt++;
+						break;
+					}
+				}
+			}
+			if(cnt>max) {
+				max=cnt;
+				answer=i;
+			}
+		}
 		return answer;
 	}
-
 }
