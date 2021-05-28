231e9e9b4833fab6e28c31f95e86b9053c73d620
nice0001-angelo <nice0001@gmail.com>
2021-05-28 02:00:50 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_3_Factorial.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.graph;
+
+/**
+ * @author njh
+ *
+ */
+public class _3_Factorial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
