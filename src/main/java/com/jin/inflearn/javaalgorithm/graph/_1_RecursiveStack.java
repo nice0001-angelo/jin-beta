92e0e9e1c0c5dc13e842c9009c465b7f4c70225c
nice0001-angelo <nice0001@gmail.com>
2021-05-28 01:19:10 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_1_RecursiveStack.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_1_RecursiveStack.java
@@ -13,11 +13,14 @@ public class _1_RecursiveStack {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		_1_RecursiveStack T = new _1_RecursiveStack();
+		T.DFS(3);
 
 	}
 
 	public void DFS(int n) {
 		
+		System.out.println(n);
+		DFS(n-1); //자기가 자기를 호출함 재귀함수 Recursive: 재귀함수는 반복문의 형태
 	}
 }
