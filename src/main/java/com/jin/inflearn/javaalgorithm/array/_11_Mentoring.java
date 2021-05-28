4a332a54348130634cea0fe7904958b0c265e41d
nice0001-angelo <nice0001@gmail.com>
2021-05-28 00:52:11 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_11_Mentoring.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_11_Mentoring.java
@@ -17,19 +17,24 @@ public class _11_Mentoring {
 	public static void main(String[] args) {
 		_11_Mentoring T = new _11_Mentoring();
 		Scanner kb = new Scanner(System.in);
-		int n = kb.nextInt();
-		int m = kb.nextInt();
-		int[][] arr = new int[m][n];
-		for(int i=0; i<m; i++) {
-			for(int j=0; j<n; j++) {
+		int stnum = kb.nextInt();
+		int testnum = kb.nextInt();
+		int[][] arr = new int[testnum][stnum];
+		for(int i=0; i<testnum; i++) {
+			for(int j=0; j<stnum; j++) {
 				arr[i][j]=kb.nextInt();
 			}
 		}
-		System.out.println(T.solution(n, m, arr));
+		System.out.println(T.solution(stnum, testnum, arr));
 	}
 	
-	public int solution(int n, int m, int[][] arr) {
+	public int solution(int stnum, int testnum, int[][] arr) {
 		int answer = Integer.MIN_VALUE;
+		for(int i=0; i<stnum; i++) {
+			for(int j=0; j<stnum; j++) {
+				
+			}
+		}
 		return answer;
 		
 	}
