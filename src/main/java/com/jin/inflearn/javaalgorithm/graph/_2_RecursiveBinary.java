f45c6ee75387ba5e39715687892367d28a9811de
nice0001-angelo <nice0001@gmail.com>
2021-05-28 01:45:51 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_2_RecursiveBinary.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.graph;
+
+/**
+ * @author njh
+ *
+ */
+public class _2_RecursiveBinary {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
