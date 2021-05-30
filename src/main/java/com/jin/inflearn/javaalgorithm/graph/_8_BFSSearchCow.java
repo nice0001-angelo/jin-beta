eb4d53560a6e3623fae7c4536cafc24b27193609
nice0001-angelo <nice0001@gmail.com>
2021-05-30 11:14:01 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_8_BFSSearchCow.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_8_BFSSearchCow.java
@@ -19,8 +19,8 @@ public class _8_BFSSearchCow {
 	Queue<Integer> Q = new LinkedList<Integer>();
 	
 	public int BFS(int s, int e) {
-		ch=new int[10001];
-		ch[s]=1;
+		ch=new int[10001]; //모든값을 0으로 초기화(방문안했을때는 0)
+		ch[s]=1; //시작지점값은 1로 체크(방문했을때는 1)
 		Q.offer(s);
 		int L=0;
 		while(!Q.isEmpty()){
@@ -29,6 +29,9 @@ public class _8_BFSSearchCow {
 				int x=Q.poll(); //큐에 있는 초기값을 받음(
 				for(int j=0; j<3; j++) {
 					int nx=x+dis[j]; //nx ==> next x(초기값은 s를 큐에 넣은것을 받아 왔으니 s=5 초기값이면 최초는 5)
+					if(nx>=1 && nx<=10000 && ch[nx]==0) { //ch[nx]==0 이란건 아직 방문안했다는 뜻: 초기화가 0
+						ch[nx]=1; //방문했을때는 check에 1을 세팅
+					}
 				}
 			}
 		}
