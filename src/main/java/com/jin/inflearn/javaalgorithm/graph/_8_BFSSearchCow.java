b27ffe02eb8efaf1dde00714a7dccbae4e398baf
nice0001-angelo <nice0001@gmail.com>
2021-05-30 10:30:08 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_8_BFSSearchCow.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_8_BFSSearchCow.java
@@ -3,18 +3,33 @@
  */
 package com.jin.inflearn.javaalgorithm.graph;
 
+import java.util.*;
+
+import com.jin.inflearn.javaalgorithm.array.*;
+
 /**
  * @author njh
  *
  */
 public class _8_BFSSearchCow {
 
-	/**
-	 * @param args
-	 */
+	int answer=0;
+	int[] dis= {-1,1,5};
+	int[] ch;
+	Queue<Integer> Q = new LinkedList<Integer>();
+	
+	public int BFS(int s, int e) {
+		return answer;
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		_8_BFSSearchCow T = new _8_BFSSearchCow();
+		Scanner kb = new Scanner(System.in);
+		int s=kb.nextInt();
+		int e=kb.nextInt();
+		System.out.println(T.BFS(s,e));
 	}
 
 }
