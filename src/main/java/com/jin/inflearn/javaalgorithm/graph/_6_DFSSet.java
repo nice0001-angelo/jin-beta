256d1d71d090197b64251a2119666051e6d8ff2b
nice0001-angelo <nice0001@gmail.com>
2021-05-30 05:58:50 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_6_DFSSet.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.graph;
+
+/**
+ * @author njh
+ *
+ */
+public class _6_DFSSet {
+
+	/**
+	 * @param args
+	 */
+	static int n;
+	static int[] ch;
+	
+	public void DFS(int L) {
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		_6_DFSSet T = new _6_DFSSet();
+		n=3;
+		ch=new int[n+1];
+		T.DFS(1);
+	}
+
+}
