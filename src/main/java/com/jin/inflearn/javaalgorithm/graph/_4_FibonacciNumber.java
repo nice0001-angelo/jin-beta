0ca15616cccfe31d50c92c31770382b8e51ac3e6
nice0001-angelo <nice0001@gmail.com>
2021-05-29 21:01:29 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_4_FibonacciNumber.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_4_FibonacciNumber.java
@@ -16,12 +16,12 @@ public class _4_FibonacciNumber {
 		_4_FibonacciNumber T = new _4_FibonacciNumber();
 		int n = 300;
 		int[] fibo = new int[n+1];
+		T.DFS(n);
 		for(int i=1; i<=n; i++) {
 			System.out.print(fibo[i]+" ");	
 		}
 		
 	}
-	
 	public int DFS(int n) {
 		int[] fibo = new int[n];
 		if(n==1) {
