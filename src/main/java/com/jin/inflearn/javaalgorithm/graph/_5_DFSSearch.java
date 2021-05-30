6369c2a1aca9da2874fca7c1e152739f828f2dc9
nice0001-angelo <nice0001@gmail.com>
2021-05-30 02:28:01 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_5_DFSSearch.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_5_DFSSearch.java
@@ -30,7 +30,15 @@ public class _5_DFSSearch {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		_5_DFSSearch tree = new _5_DFSSearch();
+		tree.root = new Node(1);
+		tree.root.lt = new Node(2);
+		tree.root.rt = new Node(3);
+		tree.root.lt.lt = new Node(4);
+		tree.root.lt.rt = new Node(5);
+		tree.root.rt.lt = new Node(6);
+		tree.root.rt.rt = new Node(7);
+		tree.DFS(tree.root);
 
 	}
 
