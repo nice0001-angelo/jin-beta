a1cd9cea1260054cb19b8dece15a2890036ec33c
nice0001-angelo <nice0001@gmail.com>
2021-06-01 21:03:47 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_10_BFSShortestPath.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.graph;
+
+/**
+ * @author njh
+ *
+ */
+public class _10_BFSShortestPath {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		_10_BFSShortestPath tree = new _10_BFSShortestPath();
+		tree.root = new Node(1);
+
+	}
+
+}
