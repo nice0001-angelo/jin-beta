70eb1fa94b550a813bdea2208e11c80c5901d1fd
nice0001-angelo <nice0001@gmail.com>
2021-06-03 03:13:38 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_10_BFSShortestPath.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_10_BFSShortestPath.java
@@ -8,6 +8,8 @@ package com.jin.inflearn.javaalgorithm.graph;
  *
  */
 public class _10_BFSShortestPath {
+	
+	Node root;
 
 	/**
 	 * @param args
@@ -15,9 +17,7 @@ public class _10_BFSShortestPath {
 	public static void main(String[] args) {
 		_10_BFSShortestPath tree = new _10_BFSShortestPath();
 		tree.root = new Node(1);
-
 	}
-
 }
 
 
