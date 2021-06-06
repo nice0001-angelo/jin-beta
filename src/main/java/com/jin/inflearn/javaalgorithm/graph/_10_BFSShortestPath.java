b7d55309921a3645852b63fdbc35dd5bc84715fb
nice0001-angelo <nice0001@gmail.com>
2021-06-06 09:16:19 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_10_BFSShortestPath.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_10_BFSShortestPath.java
@@ -10,8 +10,10 @@ import java.util.*;
  *
  */
 public class _10_BFSShortestPath {
-	
 	Node root;
+	public int BFS(Node root) {
+		Queue<Node> Q = new LinkedList<Node>();
+	}
 
 	/**
 	 * @param args
@@ -19,8 +21,11 @@ public class _10_BFSShortestPath {
 	public static void main(String[] args) {
 		_10_BFSShortestPath tree = new _10_BFSShortestPath();
 		tree.root = new Node(1);
-		Scanner kb = new Scanner(System.in);
-		int n = kb.nextInt();
+		tree.root.lt = new Node(2);
+		tree.root.rt = new Node(3);
+		tree.root.lt.lt = new Node(4);
+		tree.root.lt.rt = new Node(5);
+		System.out.println(tree.BFS(tree.root));
 	}
 }
 
