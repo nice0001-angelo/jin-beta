79c008141dffc596f6f99b5bdd90e45b4e020789
nice0001-angelo <nice0001@gmail.com>
2021-06-07 01:27:37 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_10_BFSShortestPath.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_10_BFSShortestPath.java
@@ -9,6 +9,21 @@ import java.util.*;
  * @author njh
  *
  */
+
+
+/* class Node는 이미 _5_DFSSearch에서 선언한 클래스 이므로 여기서 다시 선언할 필요가 없다
+ * class Node{ 
+ * int data; 
+ * Node lt, rt;
+ * 
+ * public Node(int val) { //Constructor
+ * data=val; 
+ * lt=rt=null; 
+ * } 
+ * }
+ */
+
+
 public class _10_BFSShortestPath {
 	Node root;
 	public int BFS(Node root) {
