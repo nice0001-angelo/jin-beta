2e57e30d86be02e3ef34833c94dd3c159160d5d0
nice0001-angelo <nice0001@gmail.com>
2021-06-09 05:35:07 -0400

    add

---
+++ src/main/java/com/interveiw/amazon/assessment/Encode.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.interveiw.amazon.assessment;
+
+/**
+ * @author njh
+ *
+ */
+public class Encode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String encode(String input) {
+		if(input == null|| input.length()==0) {
+			return "Please enter String value";
+		}
+		char[] inputArray = input.toCharArray();
+		char prevArray = 0;
+		int counter = 0;
+		
+		
+	}
+}
