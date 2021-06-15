8b64d164e849b07198f20b4792667f07ba2002a9
nice0001-angelo <nice0001@gmail.com>
2021-06-15 10:38:25 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/graph/_7_BFSSearch.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/graph/_7_BFSSearch.java
@@ -57,8 +57,7 @@ public class _7_BFSSearch {
 		tree.root.rt = new Node(3);
 		tree.root.lt.lt = new Node(4);
 		tree.root.lt.rt = new Node(5);
-		tree.root.rt.lt = new Node(6);
-		tree.root.rt.rt = new Node(7);
+		
 		tree.BFS(tree.root);
 	}
 
