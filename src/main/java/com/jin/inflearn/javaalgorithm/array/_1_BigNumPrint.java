0963aeed91eb95602503e7bb70608a2d3b298a7c
nice0001-angelo <nice0001@gmail.com>
2021-06-17 22:11:19 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_1_BigNumPrint.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_1_BigNumPrint.java
@@ -30,11 +30,14 @@ public class _1_BigNumPrint {
 	 */
 	
 	public static void main(String[] args) {
-
+		_1_BigNumPrint T = new _1_BigNumPrint();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		System.out.println("n: "+n);
 
 	}
 
-	public ArrayList<Integer> solution(int n, int[] arr) {
-
+	public void solution(int n, int[] arr) {
+		return;
 	}
 }
