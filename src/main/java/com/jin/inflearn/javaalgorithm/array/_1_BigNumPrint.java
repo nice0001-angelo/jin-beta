5208cb9bac8469f4d4cded73eed0b07c8b38daf0
nice0001-angelo <nice0001@gmail.com>
2021-06-17 22:08:47 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/array/_1_BigNumPrint.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/array/_1_BigNumPrint.java
@@ -14,30 +14,27 @@ public class _1_BigNumPrint {
 	/**
 	 * @param args
 	 */
+	/*
+	 * public static void main(String[] args) { _1_BigNumPrint T = new
+	 * _1_BigNumPrint(); Scanner kb = new Scanner(System.in); int n = kb.nextInt();
+	 * int[] arr = new int[n]; for(int i=0; i < n; i++) { arr[i] = kb.nextInt(); }
+	 * for(int x : T.solution(n, arr)) { System.out.println(x+" "); }
+	 * System.out.println();
+	 * 
+	 * }
+	 * 
+	 * public ArrayList<Integer> solution(int n, int[] arr) { //ArrayList
+	 * ArrayList<Integer> answer = new ArrayList<>(); answer.add(arr[0]); for(int i
+	 * = 1; i < n; i++) { if(arr[i] > arr[i-1] ) { answer.add(arr[i]); } } return
+	 * answer; }
+	 */
+	
 	public static void main(String[] args) {
-		_1_BigNumPrint T = new _1_BigNumPrint();
-		Scanner kb = new Scanner(System.in);
-		int n = kb.nextInt();
-		int[] arr = new int[n];
-		for(int i=0; i < n; i++) {
-			arr[i] = kb.nextInt(); 
-		}
-		for(int x : T.solution(n, arr)) {
-			System.out.println(x+" ");			
-		}
-		System.out.println();
-		
+
+
 	}
 
 	public ArrayList<Integer> solution(int n, int[] arr) {
-		//ArrayList
-		ArrayList<Integer> answer = new ArrayList<>();
-		answer.add(arr[0]);
-		for(int i = 1; i < n; i++) {
-			if(arr[i] > arr[i-1] ) {
-				answer.add(arr[i]);
-			}
-		}
-		return answer;
+
 	}
 }
