f6a58269f915e8638a40fb114c1e1e8a556b74ba
nice0001-angelo <nice0001@gmail.com>
2021-07-06 07:18:13 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/string/_2_ToUpperLowerCase.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/string/_2_ToUpperLowerCase.java
@@ -15,6 +15,7 @@ public class _2_ToUpperLowerCase {
 	 * @param args
 	 * Input: stuDY
 	 * Output: STUdy
+	 * 대문자 ASCII 65~90, 소문자 ASCII 97~122 (소문자에서 32를 빼면 대문자가 됨)
 	 */
 	public static void main(String[] args) {
 		_2_ToUpperLowerCase T = new _2_ToUpperLowerCase();
