c6c842daad67080fb455b2d750a15c43037bdab1
nice0001-angelo <nice0001@gmail.com>
2021-07-06 08:35:39 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/string/_3_WordsInSentence.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/string/_3_WordsInSentence.java
@@ -27,10 +27,7 @@ public class _3_WordsInSentence {
 	public String solution(String str) {
 		String answer = "";
 		int m = Integer.MIN_VALUE;
-		/*
-		 * String[] s = str.split(" "); for(String x : s) { int len = x.length(); if(len
-		 * > m) { m = len; answer = x; } }
-		 */
+		
 		/*
 		 * int pos; while((pos = str.indexOf(" "))!=-1) { String tmp =
 		 * str.substring(0,pos); int len = tmp.length(); if(len > m) { m = len; answer =
