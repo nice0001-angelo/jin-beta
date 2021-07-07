8f452dc2f08e71a2e3df94a950a7ddabed8facb7
nice0001-angelo <nice0001@gmail.com>
2021-07-07 11:16:24 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/string/_13_TreeSetQuestion.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.string;
+
+import java.util.*;
+
+/**
+ * @author njh
+ *
+ */
+public class _13_TreeSetQuestion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Set<Student> student = new TreeSet<Student>();
+		
+		student.add(new Student());
+	}
+
+}
+
+class Student {
+	int id;
+	String name;
+}
