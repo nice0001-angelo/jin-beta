9b33c15cf22d906b4d5c86e50abeea3375264b74
nice0001-angelo <nice0001@gmail.com>
2021-07-09 14:59:23 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/string/_6_DeleteDupChar.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/string/_6_DeleteDupChar.java
@@ -17,19 +17,20 @@ public class _6_DeleteDupChar {
 	 */
 	public static void main(String[] args) {
 		_6_DeleteDupChar T = new _6_DeleteDupChar();
-		Scanner tb = new Scanner(System.in);
-		String str = tb.next();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
 		System.out.println(T.solution(str));
 	}
 
 	public String solution(String str) {
 		String answer = new String();
-		for(int i = 0; i < str.length(); i++) {
-			//System.out.println(str.charAt(i)+" "+i+" "+str.indexOf(str.charAt(i)));
-			if(i == str.indexOf(str.charAt(i))) {
-			answer +=str.charAt(i);	
-			}
-		}
+//		for(int i = 0; i < str.length(); i++) {
+//			//System.out.println(str.charAt(i)+" "+i+" "+str.indexOf(str.charAt(i)));
+//			if(i == str.indexOf(str.charAt(i))) {
+//			answer +=str.charAt(i);	
+//			}
+//		}
+		
 		return answer;
 	}
 }
