a9a7bb5681eedd5260a1de03d17746b328680f1f
nice0001-angelo <nice0001@gmail.com>
2021-07-09 12:59:55 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/string/_5_ReverseWords.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/string/_5_ReverseWords.java
@@ -50,11 +50,17 @@ public class _5_ReverseWords {
 			int lt = 0;
 			int rt = charArr.length-1;
 			while(lt < rt) {
-				char tmp = charArr[lt];
-				charArr[lt] = charArr[rt];
-				charArr[rt] = tmp;
-				lt++;
-				rt--;
+				if(!Character.isAlphabetic(charArr[lt])) {
+					lt++;
+				} else if(!Character.isAlphabetic(charArr[rt])) {
+					rt--;
+				} else {
+					char tmp = charArr[lt];
+					charArr[lt] = charArr[rt];
+					charArr[rt] = tmp;
+					lt++;
+					rt--;					
+				}
 			}
 		answer = String.valueOf(charArr);
 		return answer;
