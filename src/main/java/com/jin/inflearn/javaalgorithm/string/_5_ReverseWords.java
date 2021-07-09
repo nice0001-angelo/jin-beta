7e614eb0a36a74d0375e0a2428aa66859cccc06e
nice0001-angelo <nice0001@gmail.com>
2021-07-09 09:37:31 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/string/_5_ReverseWords.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/string/_5_ReverseWords.java
@@ -19,33 +19,33 @@ public class _5_ReverseWords {
 	public static void main(String[] args) {
 		_5_ReverseWords T = new _5_ReverseWords();
 		Scanner kb = new Scanner(System.in);
-		//String str = kb.nextLine();
 		String str = kb.next();
 		System.out.println(T.solution(str));
 	}
 	
 	public String solution(String str) {
-		String answer = "";
-		char[] s = str.toCharArray();
-		int lt = 0;
-		int rt = str.length()-1;
-		while(rt > lt) {
-			if(!Character.isAlphabetic(s[lt])) {
-				lt++;
-			}else if(!Character.isAlphabetic(s[rt])) {
-				rt--;
-			}else {
-				char tmp = s[lt];
-				s[lt] = s[rt];
-				s[rt] = tmp;
-				lt++;
-				rt--;
-			}
-		}
-		//answer = s.toString(); //s의 주소값이 그대로 스트링으로 리턴
-		answer = String.valueOf(s); //s의 실제 값이 리턴 s는 char[]
-		System.out.println();
-		return answer;	
+//		String answer = "";
+//		char[] s = str.toCharArray();
+//		int lt = 0;
+//		int rt = str.length()-1;
+//		while(rt > lt) {
+//			if(!Character.isAlphabetic(s[lt])) {
+//				lt++;
+//			}else if(!Character.isAlphabetic(s[rt])) {
+//				rt--;
+//			}else {
+//				char tmp = s[lt];
+//				s[lt] = s[rt];
+//				s[rt] = tmp;
+//				lt++;
+//				rt--;
+//			}
+//		}
+//		//answer = s.toString(); //s의 주소값이 그대로 스트링으로 리턴
+//		answer = String.valueOf(s); //s의 실제 값이 리턴 s는 char[]
+//		return answer;
+		
+		
 		}
 }
 
