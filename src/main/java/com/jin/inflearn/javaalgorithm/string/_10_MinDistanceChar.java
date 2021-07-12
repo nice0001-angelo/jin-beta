b65315ac9f2cf4594bd9b6c9ce115a7149932ab8
nice0001-angelo <nice0001@gmail.com>
2021-07-12 11:21:29 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/string/_10_MinDistanceChar.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/string/_10_MinDistanceChar.java
@@ -20,10 +20,6 @@ public class _10_MinDistanceChar {
 	public static void main(String[] args) {
 		_10_MinDistanceChar T = new _10_MinDistanceChar();
 		Scanner kb = new Scanner(System.in);
-//		String str = kb.next();i
-//		char t = kb.next().charAt(0);
-//		for(int x : T.solution(str, t)) {
-//			System.out.print(x+" ");
 		String str = kb.next();
 		char x = kb.next().charAt(0);
 		for(int i : T.solution(str, x)) {
@@ -32,27 +28,6 @@ public class _10_MinDistanceChar {
 	}
 	
 	public int[] solution(String str, char t) {
-//		int[] answer = new int[str.length()];
-//		int lp = 1000;
-//		for(int i = 0; i < str.length(); i++) {
-//			if(str.charAt(i) == t) {
-//				lp = 0;
-//				answer[i] = lp;
-//			} else {
-//				lp++;
-//				answer[i] = lp;
-//			}
-//		}
-//		int rp = 1000;
-//		for(int i = str.length()-1 ; i >= 0 ; i--) {
-//			if(str.charAt(i) == t) {
-//				rp = 0;
-//			} else {
-//				rp++;
-//				answer[i] = Math.min(rp, answer[i]);
-//			}
-//		}
-//		return answer;
 		int[] result = new int[str.length()];
 		int lt = 1000;
 		for(int i=0; i<str.length(); i++) {
