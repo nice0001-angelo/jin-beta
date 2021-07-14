e3d6cb81a9d4d92ce66fca04a9acf85a6ad37690
nice0001-angelo <nice0001@gmail.com>
2021-07-14 17:29:35 -0400

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.hashmaptreeset;
+
+/**
+ * @author njh
+ *
+ */
+public class _1_HashMapLeader {
+
+	/**
+	 * @param args
+	 * Input: 15
+	 * Input: BACBACCACCBDEDE
+	 * Output: C
+	 */
+	public static void main(String[] args) {
+		_1_HashMapLeader T = new _1_HashMapLeader();
+
+	}
+
+	public String Solution(int n, String str) {
+		
+	}
+}
