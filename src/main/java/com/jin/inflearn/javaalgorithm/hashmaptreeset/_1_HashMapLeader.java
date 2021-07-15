cfb6f1f959aa77ee7f052c6cb1273beee7743988
nice0001-angelo <nice0001@gmail.com>
2021-07-15 13:55:15 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
@@ -26,10 +26,14 @@ public class _1_HashMapLeader {
 	}
 
 	public char Solution(int n, String str) {
-
+		char answer;
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 		for(char c : str.toCharArray()) {
-			
+			map.put(c, map.getOrDefault(c, 0)+1); //get은 key의 value값을 가져오는 것임. key의 value를 관리함으로써 count할때 요긴하게 쓸수 있음. (A, 2) 일때는 (A, 3)으로 update
 		}
+		for(char key : map.keySet()) {
+			System.out.println(key);
+		}
+		return answer;
 	}
 }
