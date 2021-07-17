c2de6560dd903a2d1cde6655981a8c304000aefc
nice0001-angelo <nice0001@gmail.com>
2021-07-17 11:44:40 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_2_HashMapAnagram.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_2_HashMapAnagram.java
@@ -3,6 +3,8 @@
  */
 package com.jin.inflearn.javaalgorithm.hashmaptreeset;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -13,11 +15,21 @@ public class _2_HashMapAnagram {
 	 * @param args
 	 * Input: AbaAeCe
 	 * Input: baeeACA
-	 * Output: yes
+	 * Output: YES
+	 * 
+	 * Input: abaCC
+	 * Input: CaaaB
+	 * Output: NO
 	 */
 	public static void main(String[] args) {
 		_2_HashMapAnagram T = new _2_HashMapAnagram();
-
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.Solution(str));
 	}
 
+	public String Solution(String str) {
+		String Answer = "";
+		return Answer;
+	}
 }
