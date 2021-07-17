e7061f305331651a9178627574e943dfe7d143d5
nice0001-angelo <nice0001@gmail.com>
2021-07-17 11:28:21 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_2_HashMapAnagram.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_2_HashMapAnagram.java
@@ -7,7 +7,7 @@ package com.jin.inflearn.javaalgorithm.hashmaptreeset;
  * @author njh
  *
  */
-public class HashMapAnagram {
+public class _2_HashMapAnagram {
 
 	/**
 	 * @param args
@@ -16,7 +16,7 @@ public class HashMapAnagram {
 	 * Output: yes
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		_2_HashMapAnagram T = new _2_HashMapAnagram();
 
 	}
 
