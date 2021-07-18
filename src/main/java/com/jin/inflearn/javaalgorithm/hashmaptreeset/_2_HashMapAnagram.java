4a31fa036560ae086adc7dd8f5f09dd8d0e2b253
nice0001-angelo <nice0001@gmail.com>
2021-07-18 14:34:44 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_2_HashMapAnagram.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_2_HashMapAnagram.java
@@ -38,7 +38,7 @@ public class _2_HashMapAnagram {
 			System.out.println(map.entrySet());
 		}
 		for(char c : b.toCharArray()) {
-			if(!map.containsKey(c) || map.get(c) == 0) { //String a에 b의 원소가 없거나  count가 0면 일치하지 않는 것이므로 "NO" 리턴
+			if(!map.containsKey(c) || map.get(c) == 0) { //String a에 b의 원소가 없거나  value의 값이 0 이면 추가로 빼봐야 갯수가 b에 더 많은 것이기 때문에 "NO" 임 차근차근 그려보면 앎
 				return "NO";
 			} else {
 			    map.put(c, map.get(c)-1);
