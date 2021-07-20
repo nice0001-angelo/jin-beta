3b75eb99f872f62536f6ba35eca5fcc5ddd253e2
nice0001-angelo <nice0001@gmail.com>
2021-07-20 16:39:36 -0400

    add

---
+++ src/main/java/com/jin/polymorphism/MyController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.jin.polymorphism;
+
+/**
+ * @author njh
+ *
+ */
+public class MyController {
+	RemoteControl rc = new Television();
+	
+	public MyController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	MyController(RemoteControl rc){
+		this.rc = rc;
+		rc.turnOn();
+		rc.setVolume(5);
+	}
+	
+	public void methodA() {
+		RemoteControl rc = new Audio();
+		
+		
+	}
+	
+}
