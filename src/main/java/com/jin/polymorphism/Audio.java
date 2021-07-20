60c6f59291783c0f67978e407c8014fe4b8fe752
nice0001-angelo <nice0001@gmail.com>
2021-07-20 16:20:00 -0400

    update

--- src/main/java/com/jin/polymorphism/Audio.java
+++ src/main/java/com/jin/polymorphism/Audio.java
@@ -3,10 +3,25 @@
  */
 package com.jin.polymorphism;
 
+import org.springframework.beans.factory.annotation.*;
+
 /**
  * @author njh
  *
  */
 public class Audio implements RemoteControl{
+	private int volume;
+	
+	@Autowired
+	RemoteControl remote;
+	
+	@Override
+	public void turnOn() {
+		System.out.println("라디오 전원을 켭니다");
+	}
 
+	@Override
+	public void turnOff() {
+		System.out.println("라디오 전원을 끕니다");
+	}
 }
