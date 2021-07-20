a113c2c0015c8832d96f4b6902b28819571dfdb0
nice0001-angelo <nice0001@gmail.com>
2021-07-20 16:14:09 -0400

    update

--- src/main/java/com/jin/polymorphism/Television.java
+++ src/main/java/com/jin/polymorphism/Television.java
@@ -3,11 +3,35 @@
  */
 package com.jin.polymorphism;
 
+import org.springframework.beans.factory.annotation.*;
+
 /**
  * @author njh
  *
  */
 public class Television implements RemoteControl{
 	
+	@Autowired
+	RemoteControl remoteControl;
+	
+	private int volume;
+	
+	@Override
+	public void turnOn() {
+		System.out.println("TV를 켭니다");
+	}
 
+	@Override
+	public void turnOff() {
+		System.out.println("TV를 끕니다");
+	}
+	
+	@Override
+	public void setVolume(int volume) {
+		if(volume > remoteControl.MAX_VOLUME) {
+			this.volume = remoteControl.MAX_VOLUME;
+		} else if(volume < remoteControl.MIN_VOLUME){
+			this.volume = remoteControl.MIN_VOLUME;
+		}
+	}
 }
