1667735d695e6c472ce5a511d16fc7bd1f5443d7
nice0001-angelo <nice0001@gmail.com>
2021-07-19 21:56:43 -0400

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_3_HashSlidingWindow.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_3_HashSlidingWindow.java
@@ -3,6 +3,8 @@
  */
 package com.jin.inflearn.javaalgorithm.hashmaptreeset;
 
+import java.util.*;
+
 /**
  * @author njh
  *
@@ -17,6 +19,8 @@ public class _3_HashSlidingWindow {
 	 */
 	public static void main(String[] args) {
 		_3_HashSlidingWindow T = new _3_HashSlidingWindow();
-		
+		Scanner kb = new Scanner(System.in);
+		int a = kb.nextInt();
+		int b = kb.nextInt();
 	}
 }
