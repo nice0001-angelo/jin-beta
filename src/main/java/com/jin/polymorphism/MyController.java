ab79e73c9904f807acc019306ecb2ef0a527c16f
nice0001-angelo <nice0001@gmail.com>
2021-07-21 00:38:11 -0400

    update

--- src/main/java/com/jin/polymorphism/MyController.java
+++ src/main/java/com/jin/polymorphism/MyController.java
@@ -10,25 +10,29 @@ package com.jin.polymorphism;
 public class MyController {
 	RemoteControl rc = new Television();
 	
+	//생성자
 	public MyController() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	//생성자(아규먼트로 넣어서 객체 주입함)
 	MyController(RemoteControl rc){
 		this.rc = rc;
 		rc.turnOn();
-		rc.setVolume(5);
+		rc.setVolume(6);
 	}
 	
+	//MethodA: New Audio 객체 주입
 	public void methodA() {
 		RemoteControl rc = new Audio();
 		rc.turnOn();
 		rc.setVolume(5);
 	}
 	
+	//MethodB: 메서드에 객체주입과 동시에 넣음
 	public void methodB(RemoteControl rc) {
 		rc.turnOn();
-		rc.setVolume(5);
+		rc.setVolume(4);
 	}
 	
 }
--- src/main/java/com/jin/polymorphism/MyPolymorphism.java
+++ src/main/java/com/jin/polymorphism/MyPolymorphism.java
@@ -17,20 +17,23 @@ public class MyPolymorphism {
 
 		System.out.println("1)----------------------");
 		
+		//기본 생성자: 예상 TV
 		MyController myController1 = new MyController();
 		myController1.rc.turnOn();
-		myController1.rc.setVolume(5);
+		myController1.rc.setVolume(7);
 				
 		
 		
 		System.out.println("2)----------------------");
 		
+		//생성자 객체주입: 예상 Audio
 		MyController myController2 = new MyController(new Audio());
 				
 		
 		
 		System.out.println("3)----------------------");
 		
+		//메소드 A: 메소드 A에 사전 정의 되어 있는데로 객체주입 : 예상 Audio
 		MyController myController3 = new MyController();
 		myController3.methodA();
 		
@@ -38,6 +41,7 @@ public class MyPolymorphism {
 		
 		System.out.println("4)----------------------");
 		
+		//메소드 B 객체주입: 예상 TV
 		MyController myController4 = new MyController();
 		myController4.methodB(new Television());
 
