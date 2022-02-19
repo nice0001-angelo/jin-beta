71961411c62a59faa66eca3ddc9615ae323ecd57
nice0001-angelo <nice0001@gmail.com>
2022-02-18 22:49:14 -0500

    _4_HashSlidingWindowAnagram: public int solution(String a, String b)

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_4_HashSlidingWindowAnagram.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_4_HashSlidingWindowAnagram.java
@@ -9,14 +9,14 @@ import java.util.*;
  * @author njh
  *
  */
-public class _4_HashSlidingWondow {
+public class _4_HashSlidingWindowAnagram {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		_4_HashSlidingWondow T = new _4_HashSlidingWondow();
+		_4_HashSlidingWindowAnagram T = new _4_HashSlidingWindowAnagram();
 		Scanner kb = new Scanner(System.in);
 		String a = kb.next();
 		String b = kb.next();
@@ -27,17 +27,11 @@ public class _4_HashSlidingWondow {
 		int answer = 0;
 		HashMap<Character, Integer> am = new HashMap<>();
 		HashMap<Character, Integer> bm = new HashMap<>();
-		for(char x : b.toCharArray()) {
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-
-
-
-
-		}
+		for(char x : b.toCharArray()) bm.put(x, bm.getOrDefault(x, 0)+1);
+		int L=b.length()-1;
+		for(int i=0; i<L; i++)
+		
+		return answer;
 	}
 
 }
