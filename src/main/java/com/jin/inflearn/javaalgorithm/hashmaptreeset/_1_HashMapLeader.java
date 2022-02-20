5a4baaf6758e073c6701b887bb86ba9bff98b0c0
nice0001-angelo <nice0001@gmail.com>
2022-02-19 23:09:17 -0500

    _1_HashMapLeader: refactoring

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
@@ -32,11 +32,11 @@ public class _1_HashMapLeader {
 		for (char x : str.toCharArray()) {
 			hashMap.put(x, hashMap.getOrDefault(x, 0)+1); //Input String을 한자 한자 따로 떼어서 hashMap에 key로 넣고 Value에는 key의 갯수를 넣는 Code.
 		}
-		System.out.println("A 가 key 중의 하나인가: "+ hashMap.containsKey('A'));
-		System.out.println("Map의 key의 갯수는: "+hashMap.size());
+		//System.out.println("A 가 key 중의 하나인가: "+ hashMap.containsKey('A'));
+		//System.out.println("Map의 key의 갯수는: "+hashMap.size());
 		int max = Integer.MIN_VALUE;
 		for(char key : hashMap.keySet()) {
-			System.out.println(key+" "+hashMap.get(key));
+			System.out.println(key+" "+hashMap.get(key)); //key와 key의 Value를 return하는 code
 			if(hashMap.get(key) > max) {
 				max = hashMap.get(key); // max에는 key에 해당하는 value의 값을 넣고
 				answer = key; // key 자체를 리턴
