98b8c2093bb9eb7a1acca733e0a8afbd91fd8bc2
nice0001-angelo <nice0001@gmail.com>
2022-02-20 13:35:24 -0500

    _1_HashMapLeader: update comments

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
@@ -28,10 +28,15 @@ public class _1_HashMapLeader {
 
 	public char Solution(int n, String str) {
 		char answer=' ';
+
+		HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
 		
 		for(char x : str.toCharArray()) {
-			HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
+
 			hashMap.put(x, hashMap.getOrDefault(x, 0)+1); //hashMap 객체에 key와 key의 Value를 넣는다
+		}
+		for(int i=0; i<hashMap.size(); i++) {
+			System.out.println(hashMap.keySet()+":"+hashMap.get(i));
 		}
 		return answer;
 	}
