f94e00741b60650abe01f01c1c57117d2b126d01
nice0001-angelo <nice0001@gmail.com>
2022-02-20 13:50:51 -0500

    _1_HashMapLeader: refactoring

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
@@ -38,6 +38,7 @@ public class _1_HashMapLeader {
 		
 		System.out.println(hashMap);
 		System.out.println(hashMap.keySet());
+		System.out.println(hashMap.containsKey('A'));
 //		for(int i=0; i<hashMap.size(); i++) {
 //			System.out.println(+":"+hashMap.get(i));
 //		}
