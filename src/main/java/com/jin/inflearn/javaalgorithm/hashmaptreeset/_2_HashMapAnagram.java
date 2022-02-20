384ce15263100c609be6cb20414e6bb92b1be016
nice0001-angelo <nice0001@gmail.com>
2022-02-20 14:42:52 -0500

    _2_HashMapAnagram: refactoring

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_2_HashMapAnagram.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_2_HashMapAnagram.java
@@ -13,12 +13,10 @@ public class _2_HashMapAnagram {
 
 	/**
 	 * @param args
-	 * Input: AbaAeCe
-	 * Input: baeeACA
+	 * Input: AbaAeCe baeeACA
 	 * Output: YES
 	 * 
-	 * Input: abaCC
-	 * Input: CaaaB
+	 * Input: abaCC CaaaB
 	 * Output: NO
 	 */
 	public static void main(String[] args) {
@@ -30,20 +28,26 @@ public class _2_HashMapAnagram {
 	}
 
 	public String Solution(String a, String b) {
-		String Answer = "YES";
-		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-		System.out.println(map.entrySet());
-		for(char c : a.toCharArray()) {
-			map.put(c, map.getOrDefault(c, 0)+1);
-			System.out.println(map.entrySet());
-		}
-		for(char c : b.toCharArray()) {
-			if(!map.containsKey(c) || map.get(c) == 0) { //String a에 b의 원소가 없거나  value의 값이 0 이면 추가로 빼봐야 갯수가 b에 더 많은 것이기 때문에 "NO" 임 차근차근 그려보면 앎
-				return "NO";
-			} else {
-			    map.put(c, map.get(c)-1);
-			}
-		}
-		return Answer;
+//		String Answer = "YES";
+//		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+//		System.out.println(map.entrySet());
+//		for(char c : a.toCharArray()) {
+//			map.put(c, map.getOrDefault(c, 0)+1);
+//			System.out.println(map.entrySet());
+//		}
+//		for(char c : b.toCharArray()) {
+//			if(!map.containsKey(c) || map.get(c) == 0) { //String a에 b의 원소가 없거나  value의 값이 0 이면 추가로 빼봐야 갯수가 b에 더 많은 것이기 때문에 "NO" 임 차근차근 그려보면 앎
+//				return "NO";
+//			} else {
+//			    map.put(c, map.get(c)-1);
+//			}
+//		}
+//		return Answer;
+		
+		
+		String answer = "Yes";
+		 
+		
+		return answer;
 	}
 }
