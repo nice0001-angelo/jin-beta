11874c23e4986ebb1de9df3c1118a8c080645b34
nice0001-angelo <nice0001@gmail.com>
2022-02-20 00:10:59 -0500

    _1_HashMapLeader: update comments

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_1_HashMapLeader.java
@@ -27,20 +27,11 @@ public class _1_HashMapLeader {
 	}
 
 	public char Solution(int n, String str) {
-		char answer = ' ';
-		HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
-		for (char x : str.toCharArray()) {
-			hashMap.put(x, hashMap.getOrDefault(x, 0)+1); //Input String을 한자 한자 따로 떼어서 hashMap에 key로 넣고 Value에는 key의 갯수를 넣는 Code.
-		}
-		//System.out.println("A 가 key 중의 하나인가: "+ hashMap.containsKey('A'));
-		//System.out.println("Map의 key의 갯수는: "+hashMap.size());
-		int max = Integer.MIN_VALUE; //max를 Integer MIN_VALUE로 초기화
-		for(char key : hashMap.keySet()) {
-			System.out.println(key+" "+hashMap.get(key)); //key와 key의 Value를 return하는 code
-			if(hashMap.get(key) > max) {
-				max = hashMap.get(key); // max에는 key에 해당하는 value의 값을 넣고
-				answer = key; // key 자체를 리턴
-			}
+		char answer=' ';
+		
+		for(char x : str.toCharArray()) {
+			HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
+			hashMap.put(x, hashMap.getOrDefault(x, 0)+1); //
 		}
 		return answer;
 	}
