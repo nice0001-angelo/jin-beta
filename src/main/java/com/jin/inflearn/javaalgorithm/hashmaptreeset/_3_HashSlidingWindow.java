e946492d74315640bf9a024008114f7049c77de5
nice0001-angelo <nice0001@gmail.com>
2022-02-21 01:14:24 -0500

    _3_HashSlidingWindow: refactoring

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_3_HashSlidingWindow.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_3_HashSlidingWindow.java
@@ -38,17 +38,26 @@ public class _3_HashSlidingWindow {
 		
 		int lt=0;
 		
-		//hashMap에 arr 값 넣어두기
+		//hashMap에 arr 값 넣기(b-1 자리수 전까지, cf b-1 자리는 rt의 자리
         for(int i=0; i<b-1; i++) {
         	hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0)+1);
         }
 		
-        //rt 값 세팅
+        //Sliding Window: 초기 lt=0, rt=b-1  ==> b가 4라면 lt=0, rt=3(총 4자리) 윈도우를 만들고 오른쪽으로 슬라이딩 시키는 로직
 		for(int rt=b-1; rt<a; rt++) {
+			//rt의 key, value 세팅
 			hashMap.put(arr[rt], hashMap.getOrDefault(arr[rt], 0)+1);
-			arrayList.add(hashMap.size());
+			
+			//key의 갯수 측정
+			arrayList.add(hashMap.size()); 
+			
+			//lt의 value -1, lt의 값 슬라이딩 전처리,,
 			hashMap.put(arr[lt], hashMap.get(arr[lt])-1);
+			
+			//lt의 value -1 후에 value가 0 가 되면 해당 key 삭제
 			if(hashMap.get(arr[lt])==0) hashMap.remove(arr[lt]);
+			
+			//lt 위치 오른쪽으로 sliding
 			lt++;
 		}
 		return arrayList;
