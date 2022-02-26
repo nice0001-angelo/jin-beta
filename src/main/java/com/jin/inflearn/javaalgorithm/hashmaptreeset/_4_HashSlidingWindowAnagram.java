8d1d437d172eb401f87ea8f2da2c4360a0da8b3d
nice0001-angelo <nice0001@gmail.com>
2022-02-26 00:44:42 -0500

    update

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_4_HashSlidingWindowAnagram.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_4_HashSlidingWindowAnagram.java
@@ -25,14 +25,19 @@ public class _4_HashSlidingWindowAnagram {
 	}
 	
 	public int solution(String a, String b) {
+		
 		int answer = 0;
+		
 		HashMap<Character, Integer> am = new HashMap<>();
 		HashMap<Character, Integer> bm = new HashMap<>();
+		
 		for(char x : b.toCharArray()) bm.put(x, bm.getOrDefault(x, 0)+1);
-		int L=b.length()-1;
-		for(int i=0; i<L; i++) am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0)+1);
+		
+		for(int i=0; i<b.length()-1; i++) am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0)+1);
+		
 		int lt=0;
-		for(int rt=L; rt<a.length(); rt++) {
+		
+		for(int rt=b.length()-1; rt<a.length(); rt++) {
 			am.put(a.charAt(rt), am.getOrDefault(a.charAt(rt), 0)+1);
 			if(am.equals(bm)) answer++;
 			am.put(a.charAt(lt), am.get(a.charAt(lt)-1));
