46c53e4b7f0bead6f8fd4483b7b208be656374ea
nice0001-angelo <nice0001@gmail.com>
2022-03-07 12:28:41 -0500

    update Treeset

--- src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_5_BigNumberK.java
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_5_BigNumberK.java
@@ -7,10 +7,13 @@ import java.util.*;
 
 /**
  * @author njh
- * 
+ * Input
  * 10 3
  * 13 15 34 23 45 65 33 26 42
+ * Output
+ * 143
  * 
+ * 중복 제거 위해서는,, 긜고 Treeset는 정렬까지 해줌 ==> 결론 정렬+중복제거 ==> TreeSet
  */
 public class _5_BigNumberK {
 
@@ -21,12 +24,14 @@ public class _5_BigNumberK {
 		_5_BigNumberK T = new _5_BigNumberK();
 		Scanner kb = new Scanner(System.in);
 		int a = kb.nextInt();
-		T.solution()
+		int b = kb.nextInt();
+		int[] arr = new int[a];
+		T.solution();
 
 	}
 	
 	
-	public int solution() {
+	public int solution(int arr[], int n,  int k) {
 		int answer;
 		
 		return answer;
