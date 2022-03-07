e812b418f48a942dcde40616db5a9e054b017be0
nice0001-angelo <nice0001@gmail.com>
2022-03-06 22:31:09 -0500

    update

---
+++ src/main/java/com/jin/inflearn/javaalgorithm/hashmaptreeset/_5_BigNumberK.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.jin.inflearn.javaalgorithm.hashmaptreeset;
+
+import java.util.*;
+
+/**
+ * @author njh
+ * 
+ * 10 3
+ * 13 15 34 23 45 65 33 26 42
+ * 
+ */
+public class _5_BigNumberK {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner tb = new Scanner(file.in);
+
+	}
+
+}
