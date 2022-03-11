03f13f338ade2abfe5be725a12db1e0de8939cc5
nice0001-angelo <nice0001@gmail.com>
2022-03-11 18:22:53 -0500

    update Junit

---
+++ src/test/java/inflearn/springcorebasic/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package inflearn.springcorebasic.hello.core.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+import inflearn.springcorebasic.hello.core.member.*;
+
+/**
+ * @author njh
+ *
+ */
+public class OrderServiceTest {
+	
+	MemberService memberService = new MemberServiceImpl();
+	OrderService orderService = new OrderServiceImpl();
+	
+	@Test
+	void creatOrder() {
+		Long memberId = 1L;
+		Member member = new Member(memberId, "memberA",Grade.VIP);
+		memberService.join(member);
+		
+		Order order = orderService.createOrder(memberId, "itemA", 10000);
+		
+		Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+	}
+}
