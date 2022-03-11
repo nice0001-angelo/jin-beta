08e138c7235168d238a22e8ee4ea431d695c0b78
nice0001-angelo <nice0001@gmail.com>
2022-03-11 16:37:47 -0500

    update Junit

--- src/test/java/inflearn/springcorebasic/hello/core/member/MemberServiceTest.java
+++ src/test/java/inflearn/springcorebasic/hello/core/member/MemberServiceTest.java
@@ -3,6 +3,7 @@
  */
 package inflearn.springcorebasic.hello.core.member;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 
 /**
@@ -11,14 +12,21 @@ import org.junit.jupiter.api.*;
  */
 public class MemberServiceTest {
 	
+	MemberService memberService = new MemberServiceImpl();
+	
 	@Test
 	void join() {
 		//given
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		
-		//when
 		
-		//then
+		//when
+		memberService.join(member);
+		Member findMember = memberService.findMember(1L);
+		
+		
+		//then: Userful org.assertj.core.api 
+		Assertions.assertThat(member).isEqualTo(findMember);
 	}
 
 }
