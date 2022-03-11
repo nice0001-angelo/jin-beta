36c68823d9e46d6a271bd9aaa496d01ef7fcfb7c
nice0001-angelo <nice0001@gmail.com>
2022-03-11 16:27:00 -0500

    add the junit

---
+++ src/test/java/inflearn/springcorebasic/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package inflearn.springcorebasic.hello.core.member;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * @author njh
+ *
+ */
+public class MemberServiceTest {
+	
+	@Test
+	void join() {
+		//given
+		Member member = new Member(1L, "memberA", Grade.VIP);
+		
+		//when
+		
+		//then
+	}
+
+}
