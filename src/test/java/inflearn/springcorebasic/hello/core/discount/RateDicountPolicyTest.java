bdb20f4b9cb306153ee87df0dd94209b458b6078
nice0001-angelo <nice0001@gmail.com>
2022-03-13 01:37:17 -0500

    add junit with failure

--- src/test/java/inflearn/springcorebasic/hello/core/discount/RateDicountPolicyTest.java
+++ src/test/java/inflearn/springcorebasic/hello/core/discount/RateDicountPolicyTest.java
@@ -34,6 +34,13 @@ class RateDicountPolicyTest {
 	@Test
 	@DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
 	void test_f() {
+		//given
+		Member member = new Member(2L, "memberBASIC", Grade.BASIC);
 		
+		//when
+		int discount = discountPolicy.discount(member, 10000);
+		
+		//then
+		Assertions.assertThat(discount).isEqualTo(0);
 	}
 }
