51e0fdcbc487813a41d46e48db80c2f14fa6fa43
nice0001-angelo <nice0001@gmail.com>
2022-03-13 16:12:20 -0400

    update OrderServiceImpl

--- src/main/java/inflearn/springcorebasic/hello/core/order/OrderServiceImpl.java
+++ src/main/java/inflearn/springcorebasic/hello/core/order/OrderServiceImpl.java
@@ -13,8 +13,11 @@ import inflearn.springcorebasic.hello.core.member.*;
 public class OrderServiceImpl implements OrderService{
 
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	
+	//FixDiscountPolicy를 RateDicountPolicy로 변경하려면 OrderServiceImpl의 코드를 변경해야 한다. OCP 위반
+	//역할과 구현을 구분해두면 휘발유차(구현체)를 전기차(구현체)로 바꾸어도 자동차운전(역할)을 바꾸면 안된다.
 //	private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-	private final DiscountPolicy discountPolicy = new RateDicountPolicy(); //DIP 위배 구현체에도 의존하고 있다
+	private final DiscountPolicy discountPolicy = new RateDicountPolicy(); 
 	
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
