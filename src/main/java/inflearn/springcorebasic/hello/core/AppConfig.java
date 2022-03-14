8403f8a8ef3d6ed588ff72f85033446dfe2cddea
nice0001-angelo <nice0001@gmail.com>
2022-03-13 23:01:23 -0400

    refactoring

--- src/main/java/inflearn/springcorebasic/hello/core/AppConfig.java
+++ src/main/java/inflearn/springcorebasic/hello/core/AppConfig.java
@@ -9,14 +9,17 @@ import inflearn.springcorebasic.hello.core.order.*;
 
 /**
  * @author njh
- *
+ * DIP, OCP를 해결하기 위한 클래스
+ * 
  */
 public class AppConfig {
 	
+	//구현체에서 직접 인터페이스에 접근하지 않게 하자(MemberService에 MemberServiceImpl이 접근할때 생성자를 통해서 접근하도록 세팅해 줌
 	public MemberService memberService() {
 		return new MemberServiceImpl(new MemoryMemberRepository());
 	}
 
+	//구현체에서 직접 인터페이스에 접근하지 않게 하자(OrderService에 OrderServiceImpl이 접근할때 생성자를 통해서 접근하도록 세팅해 줌
 	public OrderService orderService() {
 		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
 	}
