6a2877c722860d2956a5a427f8409e3495167d86
nice0001-angelo <nice0001@gmail.com>
2022-03-16 01:16:07 -0400

    update

--- src/main/java/inflearn/springcorebasic/hello/core/AppConfig.java
+++ src/main/java/inflearn/springcorebasic/hello/core/AppConfig.java
@@ -35,10 +35,14 @@ public class AppConfig {
 	//구현체에서 해당 인터페이스에만 접근하고 다른 구현체에 접근하지 않게 하자 즉 OrderServiceImpl는 해당 인터페이스인 orderService 만 접근하게 하고
 	//다른 구현체와의 관계는 AppConfig에서 접근하도록(생성자 이용: OrderServiceImpl에 필드 및 생성자를 만들어서 관계를 맺게 한다)
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
 	}
 	
 	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
+	}
+	
+	public DiscountPolicy discountPolicy() {
+		return new RateDicountPolicy();
 	}
 }
